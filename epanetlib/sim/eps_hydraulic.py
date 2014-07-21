8feb56f2f111f38a54e249f0b9cdac78f5e65481
Katherine A Klise <kaklise@sandia.gov>
2014-07-21 22:42:14 +0000

    added unit convert to hydraulic graph attributes

--- epanetlib/sim/eps_hydraulic.py
+++ epanetlib/sim/eps_hydraulic.py
@@ -1,8 +1,9 @@
 import epanetlib.pyepanet as pyepanet
 from epanetlib.network import epanet_to_MultiDiGraph
 import numpy as np
+from epanetlib.units import convert
 
-def eps_hydraulic(enData, G=None):
+def eps_hydraulic(enData, G=None, convert_units=True):
     
     if G == None:
         G = epanet_to_MultiDiGraph(enData)
@@ -24,13 +25,33 @@ def eps_hydraulic(enData, G=None):
             G.graph['time'] = G.graph['time'] + [t]
             for i in G.nodes():
                 nodeindex = enData.ENgetnodeindex(i)
-                G.node[i]['head'].append(enData.ENgetnodevalue(nodeindex, pyepanet.EN_HEAD))
-                G.node[i]['demand'].append(enData.ENgetnodevalue(nodeindex, pyepanet.EN_DEMAND))
+                
+                head = enData.ENgetnodevalue(nodeindex, pyepanet.EN_HEAD)
+                demand = enData.ENgetnodevalue(nodeindex, pyepanet.EN_DEMAND)
+                
+                if convert_units:
+                    head = convert('Hydraulic Head', G.graph['flowunits'], head) # m
+                    demand = convert('Demand', G.graph['flowunits'], demand) # m3/s
+                
+                G.node[i]['head'].append(head)
+                G.node[i]['demand'].append(demand)
+                
             for u,v,k in G.edges(keys=True):
                 linkindex = enData.ENgetlinkindex(k)
-                G[u][v][k]['flow'].append(enData.ENgetlinkvalue(linkindex, pyepanet.EN_FLOW))
-                G[u][v][k]['velocity'].append(enData.ENgetlinkvalue(linkindex, pyepanet.EN_VELOCITY))
-                G[u][v][k]['headloss'].append(enData.ENgetlinkvalue(linkindex, pyepanet.EN_HEADLOSS))
+                
+                flow = enData.ENgetlinkvalue(linkindex, pyepanet.EN_FLOW)
+                velocity = enData.ENgetlinkvalue(linkindex, pyepanet.EN_VELOCITY)
+                headloss = enData.ENgetlinkvalue(linkindex, pyepanet.EN_HEADLOSS)
+                
+                if convert_units:
+                    flow = convert('Flow', G.graph['flowunits'], flow) # m3/s
+                    velocity = convert('Velocity', G.graph['flowunits'], velocity) # m/s
+                    headloss = convert('Hydraulic Head', G.graph['flowunits'], headloss) # m
+                
+                G[u][v][k]['flow'].append(flow)
+                G[u][v][k]['velocity'].append(velocity)
+                G[u][v][k]['headloss'].append(headloss)
+                
         tstep = enData.ENnextH()
         if tstep <= 0:
             break
