a63076431c12d709cc7c00d5e9ece937c1709599
Katherine A Klise <kaklise@sandia.gov>
2014-12-05 20:08:51 +0000

    added base demand to graph attributes

--- epanetlib/network/tests/test_epanet_to_MultiDiGraph.py
+++ epanetlib/network/tests/test_epanet_to_MultiDiGraph.py
@@ -1,5 +1,6 @@
 from nose.tools import *
 from os.path import abspath, dirname, join
+import numpy as np
 
 import sys
 sys.path.append('C:\kaklise\EPA-Resilience\Evaluation_Tool')
@@ -15,17 +16,17 @@ def test_Net1():
     convert_units=False
     G = en.network.epanet_to_MultiDiGraph(enData, convert_units)
 
-    node = {'11': {'elevation': 710.0, 'nodetype': 0, 'pos': (30.0, 70.0)}, 
-            '10': {'elevation': 710.0, 'nodetype': 0, 'pos': (20.0, 70.0)}, 
-            '13': {'elevation': 695.0, 'nodetype': 0, 'pos': (70.0, 70.0)}, 
-            '12': {'elevation': 700.0, 'nodetype': 0, 'pos': (50.0, 70.0)}, 
-            '21': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 40.0)}, 
-            '22': {'elevation': 695.0, 'nodetype': 0, 'pos': (50.0, 40.0)}, 
-            '23': {'elevation': 690.0, 'nodetype': 0, 'pos': (70.0, 40.0)}, 
-            '32': {'elevation': 710.0, 'nodetype': 0, 'pos': (50.0, 10.0)}, 
-            '31': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 10.0)}, 
+    node = {'11': {'elevation': 710.0, 'nodetype': 0, 'pos': (30.0, 70.0), 'base_demand': 150}, 
+            '10': {'elevation': 710.0, 'nodetype': 0, 'pos': (20.0, 70.0), 'base_demand': 0}, 
+            '13': {'elevation': 695.0, 'nodetype': 0, 'pos': (70.0, 70.0), 'base_demand': 100}, 
+            '12': {'elevation': 700.0, 'nodetype': 0, 'pos': (50.0, 70.0), 'base_demand': 150}, 
+            '21': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 40.0), 'base_demand': 150}, 
+            '22': {'elevation': 695.0, 'nodetype': 0, 'pos': (50.0, 40.0), 'base_demand': 200}, 
+            '23': {'elevation': 690.0, 'nodetype': 0, 'pos': (70.0, 40.0), 'base_demand': 150}, 
+            '32': {'elevation': 710.0, 'nodetype': 0, 'pos': (50.0, 10.0), 'base_demand': 100}, 
+            '31': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 10.0), 'base_demand': 100}, 
             #'2':  {'elevation': 850.0, 'nodetype': 2, 'pos': (50.0, 90.0)}, node 2 is a tank and contains additional attributes
-            '9':  {'elevation': 800.0, 'nodetype': 1, 'pos': (10.0, 70.0)}}
+            '9':  {'elevation': 800.0, 'nodetype': 1, 'pos': (10.0, 70.0), 'base_demand': np.nan}}
             
     edge = {'11': {'12': {'11':  {'linktype': 1, 'diameter': 14.0, 'length': 5280.0}}, 
                    '21': {'111': {'linktype': 1, 'diameter': 10.0, 'length': 5280.0}}}, 
@@ -62,17 +63,17 @@ def test_Net1_edge_attribute():
     convert_units=False
     G = en.network.epanet_to_MultiDiGraph(enData, convert_units, edge_attribute)
     
-    node = {'11': {'elevation': 710.0, 'nodetype': 0, 'pos': (30.0, 70.0)}, 
-            '10': {'elevation': 710.0, 'nodetype': 0, 'pos': (20.0, 70.0)}, 
-            '13': {'elevation': 695.0, 'nodetype': 0, 'pos': (70.0, 70.0)}, 
-            '12': {'elevation': 700.0, 'nodetype': 0, 'pos': (50.0, 70.0)}, 
-            '21': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 40.0)}, 
-            '22': {'elevation': 695.0, 'nodetype': 0, 'pos': (50.0, 40.0)}, 
-            '23': {'elevation': 690.0, 'nodetype': 0, 'pos': (70.0, 40.0)}, 
-            '32': {'elevation': 710.0, 'nodetype': 0, 'pos': (50.0, 10.0)}, 
-            '31': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 10.0)}, 
+    node = {'11': {'elevation': 710.0, 'nodetype': 0, 'pos': (30.0, 70.0), 'base_demand': 150}, 
+            '10': {'elevation': 710.0, 'nodetype': 0, 'pos': (20.0, 70.0), 'base_demand': 0}, 
+            '13': {'elevation': 695.0, 'nodetype': 0, 'pos': (70.0, 70.0), 'base_demand': 100}, 
+            '12': {'elevation': 700.0, 'nodetype': 0, 'pos': (50.0, 70.0), 'base_demand': 150}, 
+            '21': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 40.0), 'base_demand': 150}, 
+            '22': {'elevation': 695.0, 'nodetype': 0, 'pos': (50.0, 40.0), 'base_demand': 200}, 
+            '23': {'elevation': 690.0, 'nodetype': 0, 'pos': (70.0, 40.0), 'base_demand': 150}, 
+            '32': {'elevation': 710.0, 'nodetype': 0, 'pos': (50.0, 10.0), 'base_demand': 100}, 
+            '31': {'elevation': 700.0, 'nodetype': 0, 'pos': (30.0, 10.0), 'base_demand': 100}, 
             #'2':  {'elevation': 850.0, 'nodetype': 2, 'pos': (50.0, 90.0)}, node 2 is a tank and contains additional attributes
-            '9':  {'elevation': 800.0, 'nodetype': 1, 'pos': (10.0, 70.0)}}
+            '9':  {'elevation': 800.0, 'nodetype': 1, 'pos': (10.0, 70.0), 'base_demand': np.nan}}
     
     edge = {'11': {'12': {'11':  {'linktype': 1, 'diameter': 14.0, 'length': 5280.0,  'weight': 1}}, 
                    '21': {'111': {'linktype': 1, 'diameter': 10.0, 'length': 5280.0,  'weight': 2}}}, 
