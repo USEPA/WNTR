8cd40330770eb8ae55e6458f88d26e4298653028
kaklise <kaklise@.sandia.gov>
2015-03-10 09:34:55 -0600

    Removed wdsgraph and node class

--- epanetlib/network/__init__.py
+++ epanetlib/network/__init__.py
@@ -1,7 +1,3 @@
 from epanetlib.network.epanet_to_MultiDiGraph import *
-#from epanetlib.network.epanet_to_WDSGraph import *
 from epanetlib.network.draw_graph import *
 import epanetlib.network.networkx_extensions
-from wdsgraph import *
-from node import *
-
