9efaab82b9b15a8e31f87999c2f8307c10e0d12a
kaklise <kaklise@.sandia.gov>
2015-03-23 14:01:48 -0600

    Added ScipySimulator to test_hydraulic_simulation

--- test_hydraulic_simulation.py
+++ test_hydraulic_simulation.py
@@ -2,6 +2,7 @@ from epanetlib.network.ParseWaterNetwork import ParseWaterNetwork
 from epanetlib.network.WaterNetworkModel import *
 from epanetlib.sim.WaterNetworkSimulator import *
 from epanetlib.sim.EpanetSimulator import *
+from epanetlib.sim.ScipySimulator import *
 from epanetlib.sim.PyomoSimulator import *
 import matplotlib.pylab as plt
 import epanetlib as en
@@ -45,6 +46,10 @@ en.network.draw_graph(wn, title= wn.name)
 epanet_sim = EpanetSimulator(wn)
 epanet_results = epanet_sim.run_sim()
 
+# Simulate using Scipy
+scipy_sim = ScipySimulator(wn)
+scipy_results = scipy_sim.run_sim()
+
 # Simulate using PYOMO
 pyomo_sim = PyomoSimulator(wn)
 pyomo_results = pyomo_sim.run_sim()
@@ -55,37 +60,42 @@ print pyomo_results.node.shape
 
 plt.figure()
 epanet_results.node['demand'].plot(label='EPANET')
+scipy_results.node['demand'].plot(label='SCIPY')
 pyomo_results.node['demand'].plot(label='PYOMO')
 plt.title('Node Demand')
 plt.legend()
 
 plt.figure()
 epanet_results.node['head'].plot(label='EPANET')
+scipy_results.node['head'].plot(label='SCIPY')
 pyomo_results.node['head'].plot(label='PYOMO')
 plt.title('Node Head')
 plt.legend()
 
 plt.figure()
 epanet_results.node['pressure'].plot(label='EPANET')
+scipy_results.node['pressure'].plot(label='SCIPY')
 pyomo_results.node['pressure'].plot(label='PYOMO')
 plt.title('Node Pressure')
 plt.legend()
 
 plt.figure()
 epanet_results.link['flowrate'].plot(label='EPANET')
+scipy_results.link['flowrate'].plot(label='SCIPY')
 pyomo_results.link['flowrate'].plot(label='PYOMO')
 plt.title('Link Flowrate')
 plt.legend()
 
 plt.figure()
 epanet_results.link['velocity'].plot(label='EPANET')
+scipy_results.link['velocity'].plot(label='SCIPY')
 pyomo_results.link['velocity'].plot(label='PYOMO')
 plt.title('Link Velocity')
 plt.legend()
 
 # We could Pandas testing utilities to compare DataFrames, 
 # other options can be added to use almost_equal
-from pandas.util.testing import assert_frame_equal
-assert_frame_equal(epanet_results.node, pyomo_results.node, check_less_precise=True) 
-assert_frame_equal(epanet_results.link, pyomo_results.link, check_less_precise=True) 
+#from pandas.util.testing import assert_frame_equal
+#assert_frame_equal(epanet_results.node, pyomo_results.node, check_less_precise=True) 
+#assert_frame_equal(epanet_results.link, pyomo_results.link, check_less_precise=True) 
 
