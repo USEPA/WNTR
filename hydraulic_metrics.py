5ba62fc452f6a9e618747ffa2bb868485f1e5a7c
kaklise <kaklise@.sandia.gov>
2015-04-24 09:37:50 -0600

    Partial update to hydrulic metrics using the results object

--- hydraulic_metrics.py
+++ hydraulic_metrics.py
@@ -1,73 +1,71 @@
-
-"""
-TODO This file needs to be updated to use the WaterNetworkModel
-TODO Metrics must be updated to use NetworkResults
-"""
-
 import epanetlib as en
 import numpy as np
 import networkx as nx
 from sympy.physics import units
 import matplotlib.pyplot as plt
+import pandas as pd
 
 plt.close('all')
 
-# Define water pressure unit in meters
+## Define water pressure unit in meters
 if not units.find_unit('waterpressure'):
     units.waterpressure = 9806.65*units.Pa
-
+if not units.find_unit('gallon'):
+    units.gallon = 4*units.quart
+    
 pressure_lower_bound = 30*float(units.psi/units.waterpressure) # psi to m
 demand_factor = 0.9 # 90% of requested demand
 
-# Create enData
-enData = en.pyepanet.ENepanet()
-enData.inpfile = 'networks/Net3.inp'
-enData.ENopen(enData.inpfile,'tmp.rpt')
+inp_file = 'networks/Net3.inp'
 
-# Create MultiDiGraph and plot as an undirected network
-G = en.network.epanet_to_MultiDiGraph(enData)
-en.network.draw_graph_OLD(G.to_undirected(), title=enData.inpfile)
+# Create a water network model
+wn = en.network.WaterNetworkModel()
+parser = en.network.ParseWaterNetwork()
+parser.read_inp_file(wn, inp_file)
 
-# Setup timesteps for analysis, default = 0:EN_REPORTSTEP:EN_DURATION
-duration = enData.ENgettimeparam(en.pyepanet.EN_DURATION)
-timestep = enData.ENgettimeparam(en.pyepanet.EN_REPORTSTEP)
-tstart = 24*3600
-G.graph['time'] = range(tstart,duration+1,timestep*6)
+nHours = 48
+wn.time_options['DURATION'] = nHours*3600
+wn.time_options['HYDRAULIC TIMESTEP'] = 3600
 
-# Run hydarulic simulation and save data
-G = en.sim.eps_hydraulic(enData, G)
+# Add options and conditional controls for PDD
+wn.options['MINIMUM PRESSURE'] = 0 # m
+wn.options['NOMINAL PRESSURE'] = 40*float(units.psi/units.waterpressure) # psi to m      
+
+# Simulate using Pyomo
+sim = en.sim.PyomoSimulator(wn)
+results = sim.run_sim()
+
+nzd_junctions = wn.query_node_attribute('base_demand', np.greater, 0, node_type=en.network.Junction).keys()
+junctions = [node_name for node_name, node in wn.nodes(en.network.Junction)]
 
 # Fraction of delivered volume (FDV)
-fdv = en.metrics.fraction_delivered_volume(G, pressure_lower_bound)
-print "Average FDV: " +str(np.mean(fdv.values()))
-en.network.draw_graph_OLD(G.to_undirected(), node_attribute=fdv, 
-                      title= 'FDV', node_range=[0,1])
+#fdv = en.metrics.fraction_delivered_volume(results, pressure_lower_bound)
+#print "Average FDV: " +str(np.mean(fdv.values()))
+#en.network.draw_graph_OLD(G.to_undirected(), node_attribute=fdv, 
+#                      title= 'FDV', node_range=[0,1])
 
 # Fraction of delivered demand (FDD)
-fdd = en.metrics.fraction_delivered_demand(G, pressure_lower_bound, demand_factor)
-print "Average FDD: " +str(np.mean(fdd.values()))
-en.network.draw_graph_OLD(G.to_undirected(), node_attribute=fdd, 
-                      title= 'FDD', node_range=[0,1])
+#fdd = en.metrics.fraction_delivered_demand(G, pressure_lower_bound, demand_factor)
+#print "Average FDD: " +str(np.mean(fdd.values()))
+#en.network.draw_graph_OLD(G.to_undirected(), node_attribute=fdd, 
+#                      title= 'FDD', node_range=[0,1])
 
 # Pressure stats
-head = nx.get_node_attributes(G,'head')
-elevation = nx.get_node_attributes(G,'elevation')
-pressure = np.array(head.values()) - np.array(elevation.values(),ndmin=2).T
-pressure_regulation = float(sum(np.min(pressure,axis=1) > pressure_lower_bound))/G.number_of_nodes()
+pressure = results.node.loc[(junctions, slice(None)), 'pressure']
+pressure_regulation = float(sum(pressure.min(level=0) > pressure_lower_bound))/len(junctions)
 print "Fraction of nodes > 30 psi: " + str(pressure_regulation)
-pressure_psi = pressure*float(units.waterpressure/units.psi) # m to psi
-print "Average node pressure: " +str(np.mean(pressure_psi)) + " psi"
-attr = dict(zip(G.nodes(), list(np.min(pressure, 1) < pressure_lower_bound)))
-attr2 = dict([(k,v) for k,v in attr.iteritems() if v > 0])
-en.network.draw_graph_OLD(G.to_undirected(), node_attribute=attr2, title= 'Pressure')
+print "Average node pressure: " +str(pressure.mean()) + " m"
+attr = dict(pressure.min(level=0))
+en.network.draw_graph(wn, node_attribute=attr, node_size=40, title= 'Min pressure')
 
 # Compute population per node
-qbar = en.metrics.average_demand_perday(G) # average volume of water consumed per day, m3/day
-R = 0.757082 # average volume of water consumed per capita per day, m3/day (=200 gall/day)
+demand = results.node.loc[(junctions, slice(None)), 'demand']
+qbar = en.metrics.average_demand_perday(demand) # average volume of water consumed per day, m3/day
+R = 200*float((units.gallon/units.day)/(units.m**3/units.day)) # average volume of water consumed per capita per day, m3/day
 pop = en.metrics.population(qbar,R)
 total_population = sum(pop.values())
 print "Total population: " + str(total_population)
-en.network.draw_graph_OLD(G.to_undirected(), node_attribute=pop, node_range = [0,400],
+en.network.draw_graph(wn, node_attribute=pop, node_range = [0,400],
                       title='Population, Total = ' + str(total_population))
               
 # Compute todini index
@@ -83,17 +81,23 @@ print "  Min: " + str(np.min(todini))
 
 # Compute betweenness-centrality time 36
 t=36
-attr = dict( ((u,v,k),d['flow'][t]) for u,v,k,d in G.edges(keys=True,data=True) if 'flow' in d)
-G0 = en.network.epanet_to_MultiDiGraph(enData, edge_attribute=attr)
-bet_cen = nx.betweenness_centrality(G0)  
-bet_cen2 = dict([(k,v) for k,v in bet_cen.iteritems() if v > 0.001])
-en.network.draw_graph_OLD(G, node_attribute=bet_cen2, 
-                      title='Betweenness Centrality at time ' + str(t), node_size=40, node_range=[0.001, 0.005])
+G = wn.get_graph_copy()
+flowrate = results.link.loc[(slice(None), pd.Timedelta(hours = t)), 'flowrate']
+for index, value in flowrate.iteritems():
+    link_name = index[0]
+    link = wn.get_link(link_name)
+    if value < 0:
+        G.remove_edge(link.start_node(), link.end_node(), link_name)
+        G.add_edge(link.end_node(), link.start_node(), link_name)
+        
+bet_cen = nx.betweenness_centrality(G)
+bet_cen_trim = dict([(k,v) for k,v in bet_cen.iteritems() if v > 0.001])
+en.network.draw_graph(wn, node_attribute=bet_cen, title='Betweenness Centrality', node_size=40)
 central_pt_dom = sum(max(bet_cen.values()) - np.array(bet_cen.values()))/G.number_of_nodes()
-print "Central point dominance at time " + str(t) + ": " + str(central_pt_dom)
+print "Central point dominance: " + str(central_pt_dom)
 
 # Compute all paths at time 36, for node 185
-[S, Shat, sp, dk] = en.metrics.entropy(G0, sink=['185'])
+[S, Shat, sp, dk] = en.metrics.entropy(G, sink=['185'])
 attr = dict( ((u,v,k),1) for u,v,k,d in G.edges(keys=True,data=True))
 for k in dk.keys():
     u = k[0]
