86f53cf15ce9277170fc3ea4ef7cd2dd1e3570ca
kaklise <kaklise@.sandia.gov>
2015-04-27 09:35:28 -0600

    removed draw_graph_OLD from init

--- epanetlib/network/__init__.py
+++ epanetlib/network/__init__.py
@@ -1,5 +1,5 @@
 from WaterNetworkModel import WaterNetworkModel, Node, Link, Junction, Reservoir, Tank, Pipe, Pump, Valve, Curve
 from ParseWaterNetwork import ParseWaterNetwork
-from draw_graph import draw_graph, draw_graph_OLD
+from draw_graph import draw_graph
 from epanet_to_MultiDiGraph import epanet_to_MultiDiGraph
 from network_topography import terminal_nodes, bridges
