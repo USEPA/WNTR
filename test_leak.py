d113e0f084e11e2c8245ce5488d2b734da934859
Michael Bynum <mlbynum@verdi.srn.sandia.gov>
2015-07-01 10:29:35 -0600

    Fixing a couple bugs.

--- test_leak.py
+++ test_leak.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-inp_file = 'networks/net_test_2.inp'
+inp_file = 'networks/Net1.inp'
 
 # Create a water network model
 wn = en.network.WaterNetworkModel()
@@ -13,17 +13,15 @@ parser.read_inp_file(wn, inp_file)
 # Simulate using PYOMO
 wn.set_nominal_pressures(constant_nominal_pressure = 15.0)
 pyomo_sim = en.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
-pyomo_sim.add_leak(leak_name = 'leak1', pipe_name = 'pipe2', leak_diameter=0.2, start_time = '0 days 02:00:00', fix_time = '0 days 10:00:00')
+#pyomo_sim.add_leak(leak_name = 'leak1', pipe_name = '12', leak_diameter=0.1, start_time = '0 days 05:00:00', fix_time = '0 days 20:00:00')
+pyomo_sim.add_leak(leak_name = 'leak2', pipe_name = '10', leak_diameter=0.1, start_time = '0 days 05:00:00', fix_time = '0 days 15:00:00')
+pyomo_sim.add_leak(leak_name = 'leak3', pipe_name = '110', leak_diameter=0.1, start_time = '0 days 05:00:00', fix_time = '0 days 15:00:00')
 leak_results = pyomo_sim.run_sim()
 
 # Plot Pyomo results
 node_list = [name for name,node in wn.nodes()]
-node_list.append('leak1')
 t_step = range(len(leak_results.node['demand'][node_list[0]]))
 link_list = [name for name,link in wn.links()]
-link_list.remove('pipe2')
-link_list.append('pipe2__A')
-link_list.append('pipe2__B')
 tank_list = [name for name,node in wn.nodes(en.network.Tank)]
 
 pdf = PdfPages('leak_fig.pdf')
@@ -32,7 +30,7 @@ if len(tank_list)>0:
     fig = plt.figure(figsize=(11,6))
     ax = fig.add_subplot(111)
     for tank_name in tank_list:
-        ax.plot(t_step, leak_results.node['pressure'][tank_name],label=tank_name)
+        ax.plot(t_step, leak_results.node['head'][tank_name],label=tank_name)
     ax.set_title('Tank levels')
     ax.set_xlabel('Timestep')
     ax.set_ylabel('m')
@@ -45,8 +43,8 @@ if len(tank_list)>0:
 fig = plt.figure(figsize=(11,6))
 ax = fig.add_subplot(111)
 for node_name in node_list:
-    ax.plot(t_step, leak_results.node['pressure'][node_name],label=node_name)
-ax.set_title('Node Pressure')
+    ax.plot(t_step, leak_results.node['head'][node_name],label=node_name)
+ax.set_title('Node Head')
 ax.set_xlabel('Timestep')
 ax.set_ylabel('m')
 ax.legend(loc=0, prop={'size':9})
@@ -85,5 +83,5 @@ pdf.close()
 
 #for node_name in node_list:
 #    print '\n\n\n\n',node_name
-#    print leak_results.node['head'][node_name]
+#    print leak_results.node['pressure'][node_name]
 #    print leak_results.node['demand'][node_name]
