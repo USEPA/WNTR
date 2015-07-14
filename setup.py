8596853bf413f1f847152bd6055a4efb92ac6d36
Michael Bynum <mlbynum@verdi.srn.sandia.gov>
2015-07-14 11:44:35 -0600

    Adding pyepanet dlls to setup file for installation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from distutils.core import Extension
 
 setup(name='epanetlib',
     version='0.1',
@@ -6,8 +7,9 @@ setup(name='epanetlib',
     url='https://software.sandia.gov/git/resilience',
     license='Revised BSD',
     packages=find_packages(),
-    package_data={
-        'epanetlib/pyepanet/data/Windows': ['EN2setup.exe'],
-        },             
-    data_files=[('my_data', ['epanetlib/tests/networks_for_testing/Net1.inp'])],
+    data_files=[('my_data', ['epanetlib/tests/networks_for_testing/Net1.inp']), 
+                ('epanetlib/pyepanet/data/Linux',['epanetlib/pyepanet/data/Linux/libepanet2_amd64.so']),
+                ('epanetlib/pyepanet/data/Darwin',['epanetlib/pyepanet/data/Darwin/libepanet.dylib']),
+                ('epanetlib/pyepanet/data/Windows',['epanetlib/pyepanet/data/Windows/EN2setup.exe', 'epanetlib/pyepanet/data/Windows/epanet2.bas','epanetlib/pyepanet/data/Windows/epanet2.def','epanetlib/pyepanet/data/Windows/epanet2.dll','epanetlib/pyepanet/data/Windows/epanet2.lib','epanetlib/pyepanet/data/Windows/epanet2.pas'])],
     zip_safe=False)
+
