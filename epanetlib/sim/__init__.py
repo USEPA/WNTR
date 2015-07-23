b66b674298bafd5665e8a7ffdb06652c46b7d274
kaklise <kaklise@.sandia.gov>
2015-07-23 14:07:00 -0600

    updated init file

--- epanetlib/sim/__init__.py
+++ epanetlib/sim/__init__.py
@@ -4,6 +4,3 @@ from PyomoSimulator import PyomoSimulator
 from NetworkResults import NetResults
 from NewtonSolver import NewtonSolver
 from WaterNetworkSimulator import WaterNetworkSimulator
-
-from eps_hydraulic import eps_hydraulic
-from eps_waterqual import eps_waterqual
