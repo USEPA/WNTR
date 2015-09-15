cd4650bd57a4cba094b38f8214f6b8a1812cb550
Michael Bynum <mlbynum@verdi.srn.sandia.gov>
2015-09-15 10:48:33 -0600

    updating example scripts

--- examples/simulation_modifications.py
+++ examples/simulation_modifications.py
@@ -11,15 +11,14 @@ wn.time_options['HYDRAULIC TIMESTEP'] = 1800
 wn.time_options['REPORT TIMESTEP'] = 1800
 wn.set_nominal_pressures(constant_nominal_pressure = 15) 
 
-# Create simulation object of the PYOMO simulator
-sim = wntr.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
-
 # Define pipe leaks
-sim.add_leak(leak_name = 'leak1', pipe_name = '123', leak_diameter=0.05, 
-                   start_time = '0 days 05:00:00', fix_time = '0 days 20:00:00')
-sim.add_leak(leak_name = 'leak2', pipe_name = '225', leak_diameter=0.1, 
+wn.add_leak(leak_name = 'leak1', pipe_name = '123', leak_diameter=0.05, 
+                    start_time = '0 days 05:00:00', fix_time = '0 days 20:00:00')
+wn.add_leak(leak_name = 'leak2', pipe_name = '225', leak_diameter=0.1, 
                    start_time = '0 days 07:00:00', fix_time = '0 days 15:00:00')
 
+# Create simulation object of the PYOMO simulator
+sim = wntr.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
 
 # Define power outage at all pumps
 start_time = '0 days 05:00:00' 
