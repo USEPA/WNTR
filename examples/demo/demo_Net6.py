3452263f0a1ff302605b2cae1c60a7580785050b
Michael Bynum <mlbynum@verdi.srn.sandia.gov>
2015-09-15 09:46:07 -0600

    updating demo_Net6

--- examples/demo/demo_Net6.py
+++ examples/demo/demo_Net6.py
@@ -5,7 +5,7 @@
 
 ##### Import python packages, including WNTR
 
-# In[ ]:
+# In[1]:
 
 #get_ipython().magic(u'matplotlib inline')
 
@@ -16,12 +16,12 @@ import matplotlib.pylab as plt
 import wntr
 
 np.random.seed(12345)
-demo = True
+demo = False
 
 
 ##### Create a water network model using an EPANET inp file
 
-# In[ ]:
+# In[2]:
 
 inp_file = '../networks/Net6_skel16.inp'
 wn = wntr.network.WaterNetworkModel(inp_file)
@@ -29,7 +29,7 @@ wn = wntr.network.WaterNetworkModel(inp_file)
 
 ##### Define earthquake epicenter, magnitude, and depth
 
-# In[ ]:
+# In[3]:
 
 epicenter = (210,110) # x,y location
 magnitude = 5 # Richter scale
@@ -38,7 +38,7 @@ depth = 10000 # m, shallow depth
 
 ##### Plot location of epicenter on the network
 
-# In[ ]:
+# In[4]:
 
 wntr.network.draw_graph(wn, node_size=0, figsize=(10,8), dpi=100)
 plt.hold('True')
@@ -47,7 +47,7 @@ plt.scatter(epicenter[0], epicenter[1], s=1000, c='r', marker='*', zorder=2)
 
 ##### Generate the earthquake scenario
 
-# In[ ]:
+# In[5]:
 
 #This scenario assumes uniform pipe and soil type throughout the network.  These parameters can be set for individual pipes
 #PGA = 0.001 g (0.01 m/s2) – perceptible by people
@@ -65,21 +65,21 @@ print "Number of pipe failures: " + str(len(earthquake.pipes_to_leak))
 
 ##### Plot peak ground acceleration
 
-# In[ ]:
+# In[6]:
 
 wntr.network.draw_graph(wn, link_attribute=earthquake.pga, node_size=0, link_width=1.5, title='Peak ground acceleration', figsize=(12,8), dpi=100)
 
 
 ##### Plot repair rate (# of repairs needed per m)
 
-# In[ ]:
+# In[7]:
 
 wntr.network.draw_graph(wn, link_attribute=earthquake.repair_rate, node_size=0, link_width=1.5, title='Repair rate', figsize=(12,8), dpi=100)
 
 
 ##### Plot location of pipes with leaks
 
-# In[ ]:
+# In[8]:
 
 wntr.network.draw_graph(wn, link_attribute=earthquake.probability_of_leak, node_size=0, link_width=1.5, title='Probability of pipe failure', figsize=(12,8), dpi=100)
 
@@ -89,25 +89,25 @@ wntr.network.draw_graph(wn, link_attribute=earthquake.pipe_status, node_size=0, 
 
 ##### Add leaks to the model and simulate hydraulics
 
-# In[ ]:
+# In[9]:
 
 # The simulation uses pressure driven hydraulics and leak models to account for loss.
 wn.set_nominal_pressures(constant_nominal_pressure = 15) 
 wn.time_options['DURATION'] = 24*3600
-wn.time_options['HYDRAULIC TIMESTEP'] = 1200
-wn.time_options['REPORT TIMESTEP'] = 1200
-sim = wntr.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
+wn.time_options['HYDRAULIC TIMESTEP'] = 3600
+wn.time_options['REPORT TIMESTEP'] = 3600
 
 time_of_failure = 5 # time of failure
 duration_of_failure = 20 # Select duration of failure  
-leak_diameter = {}
 for pipe_name in earthquake.pipes_to_leak:
     # Select leak diameter, uniform dist, between 0.01 and pipe diameter 
     pipe_diameter = wn.get_link(pipe_name).diameter
-    leak_diameter[pipe_name] = np.round(np.random.uniform(0.01,0.25*pipe_diameter,1), 2)[0] 
-    # Add pipe leak to the simulator
-    sim.add_leak(leak_name = "Leak"+pipe_name, pipe_name = pipe_name, leak_diameter = leak_diameter[pipe_name], 
-                 start_time = pd.Timedelta(time_of_failure, unit='h'), fix_time = pd.Timedelta(time_of_failure + duration_of_failure, unit='h'))
+    leak_diameter = np.round(np.random.uniform(0.01,0.25*pipe_diameter,1), 2)[0] 
+    # Add pipe leak to the network
+    wn.add_leak(leak_name = "Leak"+pipe_name, pipe_name = pipe_name, leak_diameter = leak_diameter, start_time = 
+                pd.Timedelta(time_of_failure, unit='h'), fix_time = pd.Timedelta(time_of_failure + duration_of_failure, unit='h'))
+
+sim = wntr.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
 
 if demo:
     results = pickle.load(open('demo_Net6.pickle', 'rb'))
@@ -118,7 +118,7 @@ else:
 
 ##### Define top leaks for repair
 
-# In[ ]:
+# In[10]:
 
 # Plot leak demand
 plt.figure(figsize=(12,5), dpi=100)
@@ -139,19 +139,14 @@ print pipes_to_fix
 
 ##### Simulate hydraulics with repair
 
-# In[ ]:
+# In[11]:
+
+duration_of_failure = 10
+for leak_name in pipes_to_fix.index:
+    leak = wn.get_node(leak_name)
+    leak.set_fix_time(pd.Timedelta(time_of_failure+duration_of_failure, unit='h'))
 
 sim = wntr.sim.PyomoSimulator(wn,'PRESSURE DRIVEN')
-
-time_of_failure = 5 # time of failure 
-for pipe_name in earthquake.pipes_to_leak:
-    if "Leak"+pipe_name in pipes_to_fix.index:
-        duration_of_failure = time_of_failure+8
-    else:
-        duration_of_failure = 20
-    # Add pipe leak to the simulator
-    sim.add_leak(leak_name = "Leak"+pipe_name, pipe_name = pipe_name, leak_diameter = leak_diameter[pipe_name], 
-                 start_time = pd.Timedelta(time_of_failure, unit='h'), fix_time = pd.Timedelta(time_of_failure + duration_of_failure, unit='h'))
 
 if demo:
     results_repair = pickle.load(open('demo_Net6_repair.pickle', 'rb'))
@@ -162,7 +157,7 @@ else:
 
 ##### Compare results
 
-# In[ ]:
+# In[12]:
 
 wn_nodes = [node_name for node_name, node in wn.nodes(wntr.network.Junction)]
 
@@ -251,7 +246,7 @@ plt.ylim(ymin=-0.05, ymax=1.05)
 plt.ylabel('Averaege FDV')
 
 
-# In[ ]:
+# In[12]:
 
 
 
