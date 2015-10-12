66a875e6463ece65d058ffeb3ae963de47fd0d4a
kaklise <kaklise@.sandia.gov>
2015-10-12 15:02:08 -0600

    remove epanet_to_MultiDiGraph from init

--- wntr/network/__init__.py
+++ wntr/network/__init__.py
@@ -1,5 +1,4 @@
 from WaterNetworkModel import WaterNetworkModel, Node, Link, Junction, Reservoir, Tank, Pipe, Pump, Valve, Curve, LinkStatus, WaterNetworkOptions
 from ParseWaterNetwork import ParseWaterNetwork
 from draw_graph import draw_graph, custom_colormap
-from epanet_to_MultiDiGraph import epanet_to_MultiDiGraph
 from WntrMultiDiGraph import WntrMultiDiGraph
