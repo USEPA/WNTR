dda51714bceb6fd6bf4abe261d46c61e3629fe2c
kaklise <kaklise@.sandia.gov>
2016-02-26 14:27:23 -0700

    Fixed MC test.  VC and EC still fail.

--- wntr/metrics/tests/test_healthimpacts.py
+++ wntr/metrics/tests/test_healthimpacts.py
@@ -21,25 +21,23 @@ def test_mass_consumed():
     sim = wntr.sim.EpanetSimulator(wn)
     results = sim.run_sim(WQ)
     
-    nzd_junctions = wn.query_node_attribute('base_demand', np.greater, 0, 
-                                        node_type=wntr.network.Junction).keys()
-    node_results = results.node.loc[:, :, nzd_junctions]
+    junctions = [junction_name for junction_name, junction in wn.junctions()]
+    node_results = results.node.loc[:, :, junctions]
 
     MC = wntr.metrics.mass_contaminant_consumed(node_results)
     MC_timeseries = MC.sum(axis=1)
     MC_cumsum = MC_timeseries.cumsum()
-    MC_cummax = MC_cumsum.cummax()
     #MC_timeseries.to_csv('MC.txt')
 
     expected = float(39069900000/1000000) # hour 2
-    error = abs((MC_cummax[2*3600] - expected)/expected)
-    print MC_cummax[2*3600], expected, error
-    assert_less(error, 0.05) # 5% error
+    error = abs((MC_cumsum[2*3600] - expected)/expected)
+    print MC_cumsum[2*3600], expected, error
+    assert_less(error, 0.01) # 1% error
     
     expected = float(1509440000000/1000000) # hour 12
-    error = abs((MC_cummax[12*3600] - expected)/expected)
-    print MC_cummax[12*3600], expected, error
-    assert_less(error, 0.05) # 5% error
+    error = abs((MC_cumsum[12*3600] - expected)/expected)
+    print MC_cumsum[12*3600], expected, error
+    assert_less(error, 0.01) # 1% error
     
 def test_volume_consumed():
     inp_file = join(datadir,'net_test_18.inp') 
@@ -51,24 +49,23 @@ def test_volume_consumed():
     sim = wntr.sim.EpanetSimulator(wn)
     results = sim.run_sim(WQ)
     
-    nzd_junctions = wn.query_node_attribute('base_demand', np.greater, 0, 
-                                        node_type=wntr.network.Junction).keys()
-    node_results = results.node.loc[:, :, nzd_junctions]
+    junctions = [junction_name for junction_name, junction in wn.junctions()]
+    node_results = results.node.loc[:, :, junctions]
     
     VC = wntr.metrics.volume_contaminant_consumed(node_results, 0)
     VC_timeseries = VC.sum(axis=1)
     VC_cumsum = VC_timeseries.cumsum()
-    VC_timeseries.to_csv('VC.txt')
+    #VC_timeseries.to_csv('VC.txt')
     
     expected = float(156760/35.3147) # hour 2
     error = abs((VC_cumsum[2*3600] - expected)/expected)
     print VC_cumsum[2*3600], expected, error
-    #assert_less(error, 0.05) # 5% error
+    assert_less(error, 0.01) # 1% error
     
     expected = float(4867920/35.3147) # hour 12
     error = abs((VC_cumsum[12*3600] - expected)/expected)
     print VC_cumsum[12*3600], expected, error
-    #assert_less(error, 0.05) # 5% error
+    assert_less(error, 0.01) # 1% error
     
 def test_extent_contaminated():
     inp_file = join(datadir,'net_test_18.inp') 
@@ -80,22 +77,25 @@ def test_extent_contaminated():
     sim = wntr.sim.EpanetSimulator(wn)
     results = sim.run_sim(WQ)
     
+    #junctions = [junction_name for junction_name, junction in wn.junctions()]
+    #node_results = results.node.loc[:, :, junctions]
+    
     EC = wntr.metrics.extent_contaminant(results.node, results.link, wn, 0)
     EC_timeseries = EC.sum(axis=1)
     EC_cummax = EC_timeseries.cummax()
-    EC_timeseries.to_csv('EC.txt')
+    #EC_timeseries.to_csv('EC.txt')
     
     expected = float(80749.9*0.3048) # hour 2
     error = abs((EC_cummax[2*3600] - expected)/expected)
     print EC_cummax[2*3600], expected, error
-    assert_less(error, 0.05) # 5% error
+    assert_less(error, 0.01) # 1% error
     
     expected = float(135554*0.3048) # hour 12
     error = abs((EC_cummax[12*3600] - expected)/expected)
     print EC_cummax[12*3600], expected, error
-    assert_less(error, 0.05) # 5% error
+    assert_less(error, 0.01) # 1% error
         
 if __name__ == '__main__':
-    #test_mass_consumed()
+    test_mass_consumed()
     test_volume_consumed()
-    #test_extent_contaminated()
+    test_extent_contaminated()
