82632e598908b09671ee29e40d3bd763044a3cbc
Michael Bynum <michaelbynum@pal-nat186-133-161.itap.purdue.edu>
2016-06-23 13:47:53 -0400

    more comments

--- wntr/sim/HydraulicModel.py
+++ wntr/sim/HydraulicModel.py
@@ -10,6 +10,33 @@ import logging
 
 logger = logging.getLogger('wntr.sim.HydraulicModel')
 
+"""
+Smoothing is used for:
+    1.) Hazen-Williams Headloss formula
+    2.) Pumps
+    3.) PDD
+    4.) Leaks
+1.) Hazen-Williams headloss formula:
+    This smoothing looks good. 
+2.) Pumps:
+    Smoothing is needed here in case the flowrate goes negative. If C is greater than 1,
+    then no polynomial is needed. Instead, the flowrate at which the derivative of the 
+    curve equals the slope of the line used for negative flow is computed. Then the line
+    is simply connected to the curve at that point. The case in which C is less than or
+    equal to 1 still needs work. There is not guarantee the polynomial will be 
+    monotonically decreasing. However, I do have some checks in place to raise a 
+    warning if the polynomial is not monotonically decreasing.
+3.) PDD:
+    The smoothing for the PDD equation looks good. I did some analysis, and, with the 
+    current PDD slopd and delta, the smoothing polynomial looks good. Additionally, the
+    smoothing does not seem to be very sensitive to Pmin or Pnom.
+4.) Leaks:
+    Smoothing is needed here in case the pressure goes negative. The smoothing for leaks
+    really needs some work. It should be easy though, because the leak equation is very
+    similar to the PDD equation. Also, I believe we can come up with a single smoothing
+    polynomial for all leaks, similar to how smoothing is done for the Hazen-Williams
+    headloss formula.
+"""
 
 class HydraulicModel(object):
     def __init__(self, wn, pressure_driven=False):
