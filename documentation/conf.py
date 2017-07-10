f8fbdc2f917b5e9b1bc0f039c27b97dbf8766b74
kaklise <kaklise@sandia.gov>
2017-07-10 11:37:57 -0600

    Added add_stylesheet to define custom css

--- documentation/conf.py
+++ documentation/conf.py
@@ -131,6 +131,9 @@ todo_include_todos = True
 # a list of builtin themes.
 #html_theme = 'sphinx_rtd_theme'
 
+def setup(app):
+  app.add_stylesheet( "wntr.css" )
+  
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
