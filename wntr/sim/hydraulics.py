3d73e4314434c32f9c447c7a14cba312f1e41e8f
Michael Bynum <michaelbynum012@gmail.com>
2017-07-11 10:00:58 -0600

    deleting some old code

--- wntr/sim/hydraulics.py
+++ wntr/sim/hydraulics.py
@@ -1006,44 +1006,6 @@ class HydraulicModel(object):
             else:
                 self.leak_demand_residual[leak_idx] = leak_demand[leak_idx]
 
-    def correct_step(self,d_head,d_demand,d_flow,d_leak,x):
-        heads = x[:self.num_nodes]
-        demands = x[self.num_nodes:2*self.num_nodes]
-        flows = x[2*self.num_nodes:2*self.num_nodes+self.num_links]
-        leaks = x[2*self.num_nodes+self.num_links:]
-
-        for link_id in self._prv_ids:
-            end_node_id = self.link_end_nodes[link_id]
-            in_links = self.in_link_ids_for_nodes[end_node_id]
-            out_links = self.out_link_ids_for_nodes[end_node_id]
-            d_head[end_node_id] = self.valve_settings[link_id]+self.node_elevations[end_node_id] - heads[end_node_id]
-            if link_id in in_links:
-                d_flow[link_id] = demands[end_node_id] + sum(flows[out_link_id] for out_link_id in out_links) - \
-                                  sum(flows[in_link_id] for in_link_id in in_links if in_link_id!=link_id) - \
-                                  flows[link_id]
-            else:
-                d_flow[link_id] = sum(flows[in_link_id] for in_link_id in in_links) - demands[end_node_id] - \
-                                  sum(flows[out_link_id] for out_link_id in out_links if out_link_id!=link_id) - \
-                                  flows[link_id]
-            if end_node_id in self._leak_ids:
-                raise RuntimeError('Leaks at the end nodes of PRVs is not allowed.')
-                # d_flow[link_id] += leaks[end_node_id]
-        for node_id in self._tank_ids:
-            in_links = self.in_link_ids_for_nodes[node_id]
-            out_links = self.out_link_ids_for_nodes[node_id]
-            d_demand[node_id] = sum(flows[in_link_id] for in_link_id in in_links) - \
-                                sum(flows[out_link_id] for out_link_id in out_links) - demands[node_id]
-            if node_id in self._leak_ids:
-                d_demand[node_id] -= leaks[self._leak_idx[node_id]]
-        for node_id in self._reservoir_ids:
-            in_links = self.in_link_ids_for_nodes[node_id]
-            out_links = self.out_link_ids_for_nodes[node_id]
-            d_demand[node_id] = sum(flows[in_link_id] for in_link_id in in_links) - \
-                                sum(flows[out_link_id] for out_link_id in out_links) - demands[node_id]
-        d_demand[:self.num_junctions] = d_demand[:self.num_junctions] - \
-                                        self.isolated_junction_array*demands[:self.num_junctions]
-        return d_head,d_demand,d_flow,d_leak
-
     def initialize_flow(self):
         flow = 0.001*np.ones(self.num_links)
         for name, link in self._wn.links():
@@ -1652,7 +1614,6 @@ class HydraulicModel(object):
             #self.print_jacobian(difference)
 
             #raise RuntimeError('Jacobian is not correct!')
-
 
     def check_jac_for_zero_rows(self):
         for i in range(self.jacobian.shape[0]):
