92fa39b1d4c07dbc13a3d4dd7c3181e8af25c41a
kaklise <kaklise@sandia.gov>
2017-07-11 10:16:50 -0600

    removed logo

--- documentation/conf.py
+++ documentation/conf.py
@@ -163,7 +163,7 @@ else:
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'figures/wntr.png'
+#html_logo = 'figures/wntr.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
