eff7615074e1f9c6209eff52eb988afc179f1716
David Hart <dbhart@sandia.gov>
2017-11-06 10:54:06 -0700

    wntr.network.controls.Comparison is now callable

--- wntr/network/controls.py
+++ wntr/network/controls.py
@@ -56,6 +56,7 @@ class Comparison(enum.Enum):
     def func(self):
         """The function call to use for this comparison"""
         return self.value[1]
+    __call__ = func
 
     @property
     def symbol(self):
