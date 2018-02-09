35f3e11677a3ed7d6880e7d80570a219b8d8340e
kaklise <kaklise@sandia.gov>
2018-02-09 11:18:17 -0700

    Added average_expected_demand to init

--- wntr/metrics/__init__.py
+++ wntr/metrics/__init__.py
@@ -3,7 +3,7 @@ The wntr.metrics package contains methods to compute resilience, including
 hydraulic, water quality, water security, and economic metrics.  Methods to 
 compute topographic metrics are included in the wntr.network.graph module.
 """
-from wntr.metrics.hydraulic import expected_demand, fdv, fdd, todini, entropy
+from wntr.metrics.hydraulic import expected_demand, average_expected_demand, fdv, fdd, todini, entropy
 from wntr.metrics.water_quality import fdq
 from wntr.metrics.water_security import mass_contaminant_consumed, volume_contaminant_consumed, extent_contaminant
 from wntr.metrics.economic import cost, ghg_emissions, pump_energy
