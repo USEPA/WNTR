496d43cdc10f27dfeb6d2acf865431ac4abe7870
Michael Bynum <mlbynum@sandia.gov>
2018-05-03 09:33:20 -0600

    fixing pump cost test

--- wntr/metrics/__init__.py
+++ wntr/metrics/__init__.py
@@ -5,6 +5,6 @@ compute topographic metrics are included in the wntr.network.graph module.
 """
 from wntr.metrics.hydraulic import expected_demand, average_expected_demand, water_service_availability, todini_index, entropy
 from wntr.metrics.water_security import mass_contaminant_consumed, volume_contaminant_consumed, extent_contaminant
-from wntr.metrics.economic import annual_network_cost, annual_ghg_emissions, pump_energy
+from wntr.metrics.economic import annual_network_cost, annual_ghg_emissions, pump_energy, pump_cost
 from wntr.metrics.misc import query, population, population_impacted
 
