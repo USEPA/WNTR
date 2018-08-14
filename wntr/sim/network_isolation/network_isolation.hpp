4bf9259b2c29746a45eaa570d1e36c57ddc41960
Michael Bynum <michaelbynum@users.noreply.github.com>
2018-08-14 07:55:10 -0600

    adding some c++ code for detecting isolated junctions and links

---
+++ wntr/sim/network_isolation/network_isolation.hpp
@@ -0,0 +1,3 @@
+#include <set>
+
+void check_for_isolated_junctions(long *sources, int source_length, long *node_indicator, int num_nodes, long *indptr, int indptr_length, long *indices, int indices_length, long *data, int data_length, long *num_connections, int num_connections_length);
