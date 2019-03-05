eb856a84af9e3b73f1de8a593cc0771bcf920604
Michael Bynum <michaelbynum@users.noreply.github.com>
2019-03-04 21:31:09 -0700

    working on c code

--- wntr/aml/evaluator.cpp
+++ wntr/aml/evaluator.cpp
@@ -19,7 +19,7 @@ void Constraint::add_jac_rpn_term(Var* v, int term)
 }
 
 
-double evaluate(std::vector<int>* rpn, std::vector<double*>* values)
+double _evaluate(std::vector<int>* rpn, std::vector<Leaf*>* values)
 {
   double stack[rpn->size()];
   double arg1;
@@ -30,7 +30,7 @@ double evaluate(std::vector<int>* rpn, std::vector<double*>* values)
     {
       if (ndx >= 0)
 	{
-	  stack[stack_ndx] = *((*values)[ndx]);
+	  stack[stack_ndx] = ((*values)[ndx])->value;
 	  ++stack_ndx;
 	}
       else
@@ -59,12 +59,99 @@ double evaluate(std::vector<int>* rpn, std::vector<double*>* values)
 }
 
 
+Evaluator::~Evaluator()
+{
+  std::set<Constraint*>::iterator con_iter;
+  for (con_iter = con_set.begin(); con_iter != con_set.end(); ++con_iter)
+    {
+      delete (*con_iter);
+    }
+
+  std::set<Var*>::iterator var_iter;
+  for (var_iter = var_set.begin(); var_iter != var_set.end(); ++var_iter)
+    {
+      delete (*var_iter);
+    }
+
+  std::set<Param*>::iterator param_iter;
+  for (param_iter = param_set.begin(); param_iter != param_set.end(); ++param_iter)
+    {
+      delete (*param_iter);
+    }
+
+  std::set<Float*>::iterator float_iter;
+  for (float_iter = float_set.begin(); float_iter != float_set.end(); ++float_iter)
+    {
+      delete (*float_iter);
+    }
+}
+
+
+Var* Evaluator::add_var(double value)
+{
+  Var* v = new Var(value);
+  var_set.insert(v);
+  return v;
+}
+
+
+Param* Evaluator::add_param(double value)
+{
+  Param* p = new Param(value);
+  param_set.insert(p);
+  return p;
+}
+
+
+Float* Evaluator::add_float(double value)
+{
+  Float* f = new Float(value);
+  float_set.insert(f);
+  return f;
+}
+
+
+void Evaluator::remove_var(Var* v)
+{
+  var_set.erase(v);
+  delete v;
+}
+
+
+void Evaluator::remove_param(Param* p)
+{
+  param_set.erase(p);
+  delete p;
+}
+
+
+void Evaluator::remove_float(Float* f)
+{
+  float_set.erase(f);
+  delete f;
+}
+
+
+void Evaluator::remove_constraint(Constraint* c)
+{
+  con_set.erase(c);
+  delete c;
+}
+
+
+Constraint* Evaluator::add_constraint()
+{
+  Constraint* c = new Constraint();
+  con_set.insert(c);
+  return c;
+}
+
+
 void Evaluator::set_structure()
 {
   fn_rpn.clear();
-  fn_leaf_values.clear();
+  leaves.clear();
   jac_rpn.clear();
-  jac_leaf_values.clear();
   col_ndx.clear();
   row_nnz.clear();
   var_vector.clear();
@@ -79,11 +166,51 @@ void Evaluator::set_structure()
       ++ndx;
     }
 
+  row_nnz.push_back(0);
+  ndx = 0;
   std::set<Constraint*>::iterator con_iter;
   for (con_iter = con_set.begin(); con_iter != con_set.end(); ++con_iter)
     {
-      fn_rpn.push_back((*con_iter)->fn_rpn);
-      std::vector<double*> leaf_vals;
-      
+      Constraint* con = *con_iter;
+      leaves.push_back(con->leaves);
+      fn_rpn.push_back(con->fn_rpn);
+      row_nnz.push_back(row_nnz[ndx] + con->jac_rpn.size());
+      std::map<Var*, std::vector<int> >::iterator jac_rpn_iter;
+      for (jac_rpn_iter = con->jac_rpn.begin(); jac_rpn_iter != con->jac_rpn.end(); ++jac_rpn_iter)
+	{
+	  col_ndx.push_back(var_indices[jac_rpn_iter->first]);
+	  jac_rpn.push_back(jac_rpn_iter->second);
+	}
+      ++ndx;
+    }
+}
+
+
+void Evaluator::evaluate(double* array_out, int array_length_out)
+{
+  int num_cons = fn_rpn.size();
+  for (int i=0; i<num_cons; ++i)
+    {
+      array_out[i] = _evaluate(&(fn_rpn[i]), &(leaves[i]));
+    }
+}
+
+
+void Evaluator::evaluate_csr_jacobian(double* values_array_out, int values_array_length_out, int* col_ndx_array_out, int col_ndx_array_length_out, int* row_nnz_array_out, int row_nnz_array_length_out)
+{
+  int num_cons = fn_rpn.size();
+  row_nnz[0] = 0;
+  int nnz_ndx = 0;
+  int nnz;
+  for (int con_ndx=0; con_ndx<num_cons; ++con_ndx)
+    {
+      row_nnz_array_out[con_ndx+1] = row_nnz[con_ndx+1];
+      nnz = row_nnz[con_ndx+1] - row_nnz[con_ndx];
+      for (int i=0; i<nnz; ++i)
+	{
+	  values_array_out[nnz_ndx] = _evaluate(&(jac_rpn[nnz_ndx]), &(leaves[con_ndx]));
+	  col_ndx_array_out[nnz_ndx] = col_ndx[nnz_ndx];
+	  ++nnz_ndx;
+	}
     }
 }
--- wntr/aml/evaluator.hpp
+++ wntr/aml/evaluator.hpp
@@ -67,14 +67,14 @@ public:
   std::vector<int> fn_rpn;
   std::map<Var*, std::vector<int> > jac_rpn;
   std::vector<Leaf*> leaves;
-}
+};
 
 
 class Evaluator
 {
 public:
   Evaluator() = default;
-  ~Evaluator() = default;
+  ~Evaluator();
 
   Var* add_var(double value);
   Param* add_param(double value);
@@ -87,27 +87,24 @@ public:
   void remove_constraint(Constraint* c);
 
   void set_structure();
-  void release_structure();
 
   void evaluate(double* array_out, int array_length_out);
   void evaluate_csr_jacobian(double* values_array_out, int values_array_length_out, int* col_ndx_array_out, int col_ndx_array_length_out, int* row_nnz_array_out, int row_nnz_array_length_out);
 
 private:
-  std::set<Var> var_set;
-  std::set<Param> param_set;
-  std::set<Float> float_set;
-  std::set<Constraint> con_set;
+  std::set<Var*> var_set;
+  std::set<Param*> param_set;
+  std::set<Float*> float_set;
+  std::set<Constraint*> con_set;
 
   std::vector<Var*> var_vector;
   
+  std::vector<std::vector<Leaf*> > leaves;
   std::vector<std::vector<int> > fn_rpn;
-  std::vector<std::vector<double*> > fn_leaf_values;
-
   std::vector<std::vector<int> > jac_rpn;
-  std::vector<std::vector<double*> > jac_leaf_values;
   std::vector<int> col_ndx;
   std::vector<int> row_nnz;
 };
 
 
-double evaluate(std::vector<int>* rpn, std::vector<double*>* values);
+double _evaluate(std::vector<int>* rpn, std::vector<Leaf*>* values);
