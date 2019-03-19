9e02efb9e90fd048c98daf6edf35359dbbc59792
Michael Bynum <michaelbynum@users.noreply.github.com>
2019-03-19 13:27:21 -0600

    removing c++11 features

--- wntr/sim/network_isolation/network_isolation.cpp
+++ wntr/sim/network_isolation/network_isolation.cpp
@@ -2,43 +2,42 @@
 
 void check_for_isolated_junctions(long *sources, int source_length, long *node_indicator, int num_nodes, long *indptr, int indptr_length, long *indices, int indices_length, long *data, int data_length, long *num_connections, int num_connections_length)
 {
-    int source_id;
-    int node_being_explored;
-    int ndx;
-    int number_of_connections;
-    int val;
-    int col;
+  int source_id;
+  int node_being_explored;
+  int ndx;
+  int number_of_connections;
+  int val;
+  int col;
 
-    for (int source_cntr = 0; source_cntr < source_length; ++ source_cntr)
+  for (int source_cntr = 0; source_cntr < source_length; ++ source_cntr)
     {
-        source_id = sources[source_cntr];
-        if (node_indicator[source_id] == 1)
+      source_id = sources[source_cntr];
+      if (node_indicator[source_id] == 1)
         {
-            node_indicator[source_id] = 0;
-            std::set<int> nodes_to_explore;
-            nodes_to_explore.insert(source_id);
+	  node_indicator[source_id] = 0;
+	  std::set<int> nodes_to_explore;
+	  nodes_to_explore.insert(source_id);
 
-            while (!nodes_to_explore.empty())
+	  while (!nodes_to_explore.empty())
             {
-                auto nodes_to_explore_iter = nodes_to_explore.end();
-                --nodes_to_explore_iter;
-                node_being_explored = *nodes_to_explore_iter;
-                nodes_to_explore.erase(nodes_to_explore_iter);
-                ndx = indptr[node_being_explored];
-                number_of_connections = num_connections[node_being_explored];
-                for (int i = 0; i < number_of_connections; ++i)
+	      std::set<int>::iterator nodes_to_explore_iter = nodes_to_explore.end();
+	      --nodes_to_explore_iter;
+	      node_being_explored = *nodes_to_explore_iter;
+	      nodes_to_explore.erase(nodes_to_explore_iter);
+	      ndx = indptr[node_being_explored];
+	      number_of_connections = num_connections[node_being_explored];
+	      for (int i = 0; i < number_of_connections; ++i)
                 {
-                    val = data[ndx + i];
-                    if (val == 1)
+		  val = data[ndx + i];
+		  if (val == 1)
                     {
-                        col = indices[ndx + i];
-                        if (node_indicator[col] == 1)
+		      col = indices[ndx + i];
+		      if (node_indicator[col] == 1)
                         {
-                            node_indicator[col] = 0;
-                            nodes_to_explore.insert(col);
+			  node_indicator[col] = 0;
+			  nodes_to_explore.insert(col);
                         }
                     }
-
                 }
             }
         }
