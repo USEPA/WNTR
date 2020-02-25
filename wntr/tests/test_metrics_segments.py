32c87355fb16c96672c33ca58b617c85f47a812c
jhogge <56038463+jhogge@users.noreply.github.com>
2020-02-25 15:23:36 -0700

    minor changes
    - avoiding pandas deprecated testing utility

--- wntr/tests/test_metrics_segments.py
+++ wntr/tests/test_metrics_segments.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import pandas as pd
-from pandas.util.testing import assert_series_equal
+from pandas.testing import assert_series_equal
 from os.path import abspath, dirname, join
 import wntr
 
