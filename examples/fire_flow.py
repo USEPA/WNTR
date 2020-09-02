e42e2058cfa3beb8983fffd96f243d9d3067a7ca
Katherine Klise <kaklise@sandia.gov>
2020-09-02 17:57:11 -0600

    updated examples

--- examples/fire_flow.py
+++ examples/fire_flow.py
@@ -7,6 +7,7 @@ import wntr
 # Create a water network model and simulate under nominal conditions
 inp_file = 'networks/Net3.inp'
 wn = wntr.network.WaterNetworkModel(inp_file)
+wn.options.hydraulic.demand_model = 'PDD'
 
 # Add fire demand and simulate
 fire_flow_demand = 0.252 # 4000 gal/min = 0.252 m3/s
@@ -14,13 +15,13 @@ fire_start = 10*3600
 fire_end = 36*3600
 node = wn.get_node('197')
 node.add_fire_fighting_demand(wn, fire_flow_demand, fire_start, fire_end)
-sim = wntr.sim.WNTRSimulator(wn, mode='PDD')
+sim = wntr.sim.WNTRSimulator(wn)
 fire_results = sim.run_sim()
 
 # Reset initial values and simulate hydraulics under nominal conditions
 wn.reset_initial_values()
 node.remove_fire_fighting_demand(wn)
-sim = wntr.sim.WNTRSimulator(wn, mode='PDD')
+sim = wntr.sim.WNTRSimulator(wn)
 results = sim.run_sim()
 
 # Plot resulting differences on the network
