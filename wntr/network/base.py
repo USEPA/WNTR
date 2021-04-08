b5b6d1a4199a2cd26cd5f14944499fd3f59e7147
dbhart <dbhart@sandia.gov>
2021-04-08 10:34:10 -0600

    A few more updates to core files

--- wntr/network/base.py
+++ wntr/network/base.py
@@ -730,10 +730,11 @@ class NodeType(enum.IntEnum):
     Tank = 2  #: node is a tank
 
     def __init__(self, val):
-        if self.name != self.name.upper():
-            self._member_map_[self.name.upper()] = self
-        if self.name != self.name.lower():
-            self._member_map_[self.name.lower()] = self
+        mmap = getattr(self, '_member_map_')
+        if self.name != str(self.name).upper():
+            mmap[str(self.name).upper()] = self
+        if self.name != str(self.name).lower():
+            mmap[str(self.name).lower()] = self
 
     def __str__(self):
         return self.name
@@ -776,10 +777,11 @@ class LinkType(enum.IntEnum):
     Valve = 9  #: a valve of any type
 
     def __init__(self, val):
-        if self.name != self.name.upper():
-            self._member_map_[self.name.upper()] = self
-        if self.name != self.name.lower():
-            self._member_map_[self.name.lower()] = self
+        mmap = getattr(self, '_member_map_')
+        if self.name != str(self.name).upper():
+            mmap[str(self.name).upper()] = self
+        if self.name != str(self.name).lower():
+            mmap[str(self.name).lower()] = self
 
     def __str__(self):
         return self.name
@@ -816,10 +818,11 @@ class LinkStatus(enum.IntEnum):
     CV = 3  #: pipe has a check valve
 
     def __init__(self, val):
-        if self.name != self.name.upper():
-            self._member_map_[self.name.upper()] = self
-        if self.name != self.name.lower():
-            self._member_map_[self.name.lower()] = self
+        mmap = getattr(self, '_member_map_')
+        if self.name != str(self.name).upper():
+            mmap[str(self.name).upper()] = self
+        if self.name != str(self.name).lower():
+            mmap[str(self.name).lower()] = self
 
     def __str__(self):
         return self.name
--- wntr/sim/results.py
+++ wntr/sim/results.py
@@ -4,6 +4,7 @@ import copy
 from wntr.epanet.util import FlowUnits, MassUnits, HydParam, QualParam
 from wntr.epanet.util import from_si
 
+
 class ResultsStatus(enum.IntEnum):
     converged = 1
     error = 0
@@ -23,8 +24,28 @@ class SimulationResults(object):
         self.link = None
         self.node = None
 
-    def convert_units(self, flow_units='GPM', mass_units='mg', qual_param=None, 
-                      return_copy=True):
+    def __sub__(self, other):
+        if not isinstance(other, SimulationResults):
+            raise ValueError(
+                "differencing from a results object requires both be SimulationResults"
+            )
+        new = SimulationResults()
+        new.link = dict()
+        new.node = dict()
+        new.network_name = "{}[{}] - {}[{}]".format(
+            self.network_name, self.timestamp, other.network_name, other.timestamp
+        )
+        for key in self.link.keys():
+            if key in other.link:
+                new.link[key] = self.link[key] - other.link[key]
+        for key in self.node.keys():
+            if key in other.node:
+                new.node[key] = self.node[key] - other.node[key]
+        return new
+
+    def convert_units(
+        self, flow_units="GPM", mass_units="mg", qual_param=None, return_copy=True
+    ):
         """
         Convert simulation results to EPANET unit convensions.
         
@@ -50,52 +71,82 @@ class SimulationResults(object):
             results = copy.deepcopy(self)
         else:
             results = self
-            
+
         if flow_units is not None and isinstance(flow_units, str):
             flow_units = flow_units.upper()
             flow_units = FlowUnits[flow_units]
-            
+
         if mass_units is not None and isinstance(mass_units, str):
             mass_units = mass_units.lower()
             mass_units = MassUnits[mass_units]
-        
-        if qual_param is not None and isinstance(qual_param, str) and qual_param in ['CONCENTRATION', 'AGE', 'TRACE', 'NONE']:
+
+        if (
+            qual_param is not None
+            and isinstance(qual_param, str)
+            and qual_param in ["CONCENTRATION", "AGE", "TRACE", "NONE"]
+        ):
             qual_param = qual_param.upper()
-            #qual_param = QualParam[qual_param]
-        
+            # qual_param = QualParam[qual_param]
+
         ## Nodes ##
         for key in results.node.keys():
-            results.node[key].index = results.node[key].index/3600
-        
-        results.node['demand'] = from_si(flow_units, results.node['demand'], HydParam.Demand)
-        results.node['head'] = from_si(flow_units, results.node['head'], HydParam.HydraulicHead)
-        results.node['pressure'] = from_si(flow_units, results.node['pressure'], HydParam.Pressure)
-        
-        if qual_param == 'CHEMICAL':
-            results.node['quality'] = from_si(flow_units, results.node['quality'], QualParam.Concentration, mass_units=mass_units)
-        elif qual_param == 'AGE':
-            results.node['quality']  = from_si(flow_units, results.node['quality'], QualParam.WaterAge)
+            results.node[key].index = results.node[key].index / 3600
+
+        results.node["demand"] = from_si(
+            flow_units, results.node["demand"], HydParam.Demand
+        )
+        results.node["head"] = from_si(
+            flow_units, results.node["head"], HydParam.HydraulicHead
+        )
+        results.node["pressure"] = from_si(
+            flow_units, results.node["pressure"], HydParam.Pressure
+        )
+
+        if qual_param == "CHEMICAL":
+            results.node["quality"] = from_si(
+                flow_units,
+                results.node["quality"],
+                QualParam.Concentration,
+                mass_units=mass_units,
+            )
+        elif qual_param == "AGE":
+            results.node["quality"] = from_si(
+                flow_units, results.node["quality"], QualParam.WaterAge
+            )
         else:
-            pass # Trace or None, no conversion needed
-        
+            pass  # Trace or None, no conversion needed
+
         ## Links ##
         for key in self.link.keys():
-            results.link[key].index = results.link[key].index/3600
-    
-        results.link['flowrate'] = from_si(flow_units, results.link['flowrate'], HydParam.Flow)
-        results.link['headloss'] = from_si(flow_units, results.link['headloss'], HydParam.HeadLoss)
-        results.link['velocity'] = from_si(flow_units, results.link['velocity'], HydParam.Velocity)
-        
-        if qual_param == 'CHEMICAL':
-            results.link['linkquality']  = from_si(flow_units, results.link['linkquality'], QualParam.Concentration, mass_units=mass_units)
-        elif qual_param == 'AGE':
-            results.link['linkquality']  = from_si(flow_units, results.link['linkquality'], QualParam.WaterAge)
+            results.link[key].index = results.link[key].index / 3600
+
+        results.link["flowrate"] = from_si(
+            flow_units, results.link["flowrate"], HydParam.Flow
+        )
+        results.link["headloss"] = from_si(
+            flow_units, results.link["headloss"], HydParam.HeadLoss
+        )
+        results.link["velocity"] = from_si(
+            flow_units, results.link["velocity"], HydParam.Velocity
+        )
+
+        if qual_param == "CHEMICAL":
+            results.link["linkquality"] = from_si(
+                flow_units,
+                results.link["linkquality"],
+                QualParam.Concentration,
+                mass_units=mass_units,
+            )
+        elif qual_param == "AGE":
+            results.link["linkquality"] = from_si(
+                flow_units, results.link["linkquality"], QualParam.WaterAge
+            )
         else:
-            pass # Trace or None, no conversion needed
-        
+            pass  # Trace or None, no conversion needed
+
         # frictionfact no conversion needed
         # status no conversion needed
         # setting requires valve type, convert with pressure or flow type, or change setting to pressure_setting and flow_setting.
         # rxnrate, convert with BulkReactionCoeff? or WallReactionCoeff?
-        
+
         return results
