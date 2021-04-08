b51e3e91e193f9366d5be2be2e42af189e93af30
dbhart <dbhart@sandia.gov>
2021-04-08 10:03:56 -0600

    Fix another test

--- wntr/tests/test_network_minor_loss.py
+++ wntr/tests/test_network_minor_loss.py
@@ -1,10 +1,10 @@
 import unittest
-from nose import SkipTest
+from unittest import SkipTest
+
 import wntr
 
 
 class TestMinorLosses(unittest.TestCase):
-
     @classmethod
     def setUpClass(self):
         pass
@@ -16,22 +16,24 @@ class TestMinorLosses(unittest.TestCase):
     def test_pipe_minor_loss(self):
         wn = wntr.network.WaterNetworkModel()
         wn.options.time.duration = 3600 * 2
-        wn.add_reservoir(name='r1', base_head=20.0)
-        wn.add_junction(name='j1', base_demand=0.1)
-        wn.add_pipe(name='p1', start_node_name='r1', end_node_name='j1', minor_loss=100.0)
-        wn.options.hydraulic.demand_model = 'DDA'
+        wn.add_reservoir(name="r1", base_head=20.0)
+        wn.add_junction(name="j1", base_demand=0.1)
+        wn.add_pipe(
+            name="p1", start_node_name="r1", end_node_name="j1", minor_loss=100.0
+        )
+        wn.options.hydraulic.demand_model = "DDA"
         sim = wntr.sim.WNTRSimulator(wn)
 
         results1 = sim.run_sim()
-        wn.write_inpfile('temp.inp', 'CMH')
-        
-        #raise SkipTest # EPANET seg faults (on Travis)
-        
-        wn2 = wntr.network.WaterNetworkModel('temp.inp')
+        wn.write_inpfile("temp.inp", "CMH")
+
+        # raise SkipTest # EPANET seg faults (on Travis)
+
+        wn2 = wntr.network.WaterNetworkModel("temp.inp")
         sim = wntr.sim.EpanetSimulator(wn2)
         results2 = sim.run_sim()
 
-        head1 = results1.node['head'].loc[0, 'j1']
-        head2 = results2.node['head'].loc[0, 'j1']
-        head_diff = abs(head1-head2)
+        head1 = results1.node["head"].j1.iloc[0]
+        head2 = results2.node["head"].j1.iloc[0]
+        head_diff = abs(head1 - head2)
         self.assertLess(head_diff, 0.01)
