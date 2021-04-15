f8994e7663706b26d9214e15aac7e0891641c821
Katherine Klise <kaklise@sandia.gov>
2021-04-15 15:31:41 -0600

    updated test

--- wntr/tests/test_sim_results.py
+++ wntr/tests/test_sim_results.py
@@ -6,29 +6,6 @@ import wntr
 testdir = dirname(abspath(str(__file__)))
 datadir = join(testdir, "..", "..", "examples", "networks")
 
-def get_min_max(results):
-    results_min = wntr.sim.SimulationResults()
-    results_min.link = dict()
-    results_min.node = dict()
-    results_min.network_name = "-{}[{}]".format(
-        results.network_name, results.timestamp)
-    
-    results_max = wntr.sim.SimulationResults()
-    results_max.link = dict()
-    results_max.node = dict()
-    results_max.network_name = "-{}[{}]".format(
-        results.network_name, results.timestamp)
-    
-    for key in results.link.keys():
-        results_min.link[key] = results.link[key].min().min()
-        results_max.link[key] = results.link[key].max().max()
-    for key in results.node.keys():
-        results_min.node[key] = results.node[key].min().min()
-        results_max.node[key] = results.node[key].max().max()
-        
-    return results_min, results_max
-    
-
 class TestSimulationResults(unittest.TestCase):
     
     @classmethod
@@ -45,14 +22,25 @@ class TestSimulationResults(unittest.TestCase):
             results_GPM.node[key].index = results_GPM.node[key].index/3600
         for key in results_GPM.link.keys():
             results_GPM.link[key].index = results_GPM.link[key].index/3600
+
+        self.results_GPM = results_GPM
         
-        results_min, results_max = get_min_max(results_GPM)
-        results_GPM_norm = (results_GPM - results_min)/results_max
-        
-        self.results_GPM_norm = results_GPM_norm
-        
-        self.tol = 0.0001 
-        
+        self.tol = {}
+        self.tol['node'] = {}
+        self.tol['node']['demand'] = 1e-3
+        self.tol['node']['head'] = 1e-3
+        self.tol['node']['pressure'] = 1e-3
+        self.tol['node']['quality'] = 1e-3
+        self.tol['link'] = {}
+        self.tol['link']['flowrate'] = 1e-2
+        self.tol['link']['velocity'] = 1e-3
+        self.tol['link']['headloss'] = 1e-3
+        self.tol['link']['status'] = None # status is not checked.
+        self.tol['link']['setting'] = 1e-5
+        self.tol['link']['friction_factor'] = 1e-5
+        self.tol['link']['quality'] = 1e-5
+        self.tol['link']['reaction_rate'] = 1e-3
+
     @classmethod
     def tearDownClass(self):
         pass
@@ -66,26 +54,20 @@ class TestSimulationResults(unittest.TestCase):
                                                        "mg", #self.wn.options.quality.inpfile_units,
                                                        self.wn.options.quality.parameter)
         
-        results_min, results_max = get_min_max(results_convert_GPM)
-        results_convert_GPM_norm = (results_convert_GPM - results_min)/results_max
-        
-        results_diff = abs(results_convert_GPM_norm - self.results_GPM_norm)
+        results_diff = abs(results_convert_GPM - self.results_GPM)
 
         for key in results_diff.node.keys():
-            #print(key, results_diff.node[key].max().max())
             #wntr.graphics.plot_network(self.wn, node_attribute=results_diff.node[key].max(), link_width=0.5, title=key+' diff, test_convert_units_epanet')
-            self.assertLess(results_diff.node[key].max().max(), self.tol)
+            #print(key, results_diff.node[key].max().max())
+            self.assertLess(results_diff.node[key].max().max(), self.tol['node'][key])
             
         for key in results_diff.link.keys():
             #wntr.graphics.plot_network(self.wn, link_attribute=results_diff.link[key].max(), link_width=2, node_size=0, title=key+' diff, test_convert_units_wntr')
             #print(key, results_diff.link[key].max().max())
-            if key in ['status', 'reaction_rate']: # reaction rate is contant (normalized values are nan), status is different than epanet status
+            if key in ['status']: # status in WNTR is different than EPANET status
                 continue
-            self.assertLess(results_diff.link[key].max().max(), self.tol)
+            self.assertLess(results_diff.link[key].max().max(), self.tol['link'][key])
     
-    
-    
-            
     def test_convert_units_wntr(self):
         sim = wntr.sim.WNTRSimulator(self.wn)
         results_SI = sim.run_sim()
@@ -95,23 +77,20 @@ class TestSimulationResults(unittest.TestCase):
                                                        "mg", #self.wn.options.quality.inpfile_units,
                                                        None)
         
-        results_min, results_max = get_min_max(results_convert_GPM)
-        results_convert_GPM_norm = (results_convert_GPM - results_min)/results_max
-        
-        results_diff = abs(results_convert_GPM_norm - self.results_GPM_norm)
+        results_diff = abs(results_convert_GPM - self.results_GPM)
 
         for key in results_diff.node.keys():
             #wntr.graphics.plot_network(self.wn, node_attribute=results_diff.node[key].max(), link_width=0.5, title=key+' diff, test_convert_units_wntr')
             #print(key, results_diff.node[key].max().max())
-            self.assertLess(results_diff.node[key].max().max(), self.tol)
+            self.assertLess(results_diff.node[key].max().max(), self.tol['node'][key])
             
         for key in results_diff.link.keys():
             #wntr.graphics.plot_network(self.wn, link_attribute=results_diff.link[key].max(), link_width=2, node_size=0, title=key+' diff, test_convert_units_wntr')
-            #print(key, results_diff.link[key].loc[1::,:].max().max())
-            if key in 'status': 
+            print(key, results_diff.link[key].loc[1::,:].max().max())
+            if key in 'status': # status in WNTR is different than EPANET status
                 continue
             # Some differences exist in the first hour
-            self.assertLess(results_diff.link[key].loc[1::,:].max().max(), self.tol)
+            self.assertLess(results_diff.link[key].loc[1::,:].max().max(), self.tol['link'][key])
 
 
 if __name__ == "__main__":
