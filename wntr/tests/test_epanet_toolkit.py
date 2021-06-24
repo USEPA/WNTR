55607623d5b5e144803ac3d9fb002d731af41720
Katherine Klise <kaklise@sandia.gov>
2021-06-24 15:10:08 -0700

    Added tests

--- wntr/tests/test_epanet_toolkit.py
+++ wntr/tests/test_epanet_toolkit.py
@@ -20,11 +20,35 @@ class TestEpanetToolkit(unittest.TestCase):
         enData = wntr.epanet.toolkit.ENepanet()
         enData.inpfile = join(datadir, "Net1.inp")
         enData.ENopen(enData.inpfile, "temp.rpt")
+        
         nNodes = enData.ENgetcount(wntr.epanet.util.EN.NODECOUNT)
         self.assertEqual(11, nNodes)
         nLinks = enData.ENgetcount(wntr.epanet.util.EN.LINKCOUNT)
         self.assertEqual(13, nLinks)
+    
+    def test_ENgetflowunits(self):
+        enData = wntr.epanet.toolkit.ENepanet()
+        enData.inpfile = join(datadir, "Net1.inp")
+        enData.ENopen(enData.inpfile, "temp.rpt")
         
+        flowunit = enData.ENgetflowunits()
+        assert(flowunit==1) # GPM
+        
+    def test_ENgetindex_ENgetvalue(self):
+        enData = wntr.epanet.toolkit.ENepanet()
+        enData.inpfile = join(datadir, "Net1.inp")
+        enData.ENopen(enData.inpfile, "temp.rpt")
+        
+        node_index = enData.ENgetnodeindex('10') 
+        assert(node_index == 1) 
+        node_val = enData.ENgetnodevalue(node_index, 0) # ELEVATION = 0
+        assert(node_val == 710) 
+        
+        link_index = enData.ENgetlinkindex('11') 
+        assert(link_index == 2) 
+        link_val = enData.ENgetlinkvalue(link_index, 0) # DIAMETER = 0
+        assert(link_val == 14) 
+         
     def test_runepanet(self):
         inpfile = join(datadir, "Net1.inp")
         wntr.epanet.toolkit.runepanet(inpfile)
