9358527ea52739d83da0764ac182d94c730839df
Thi <thilozirnstein@gmail.com>
2022-08-22 10:47:54 +0200

    Update setup.py -> always build

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import re
 import sys
 
 use_swig = False
-build = False
+build = True
 
 extension_modules = list()
 
-if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
-    print("Python version >= 3.10.x")
-    build = True
+# if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
+#     print("Python version >= 3.10.x")
+#     build = True
 
 if build:
     import numpy
