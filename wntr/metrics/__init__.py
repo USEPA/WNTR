1d427e34fb5bd6d7c4a7d25be29e7a6786c0bb3e
kbonney <kirkb1998@gmail.com>
2022-10-12 10:12:39 -0400

    Fairly confident in dev_valve_segments in this commit. Did a basic comparison between old and new algs on Net6 and things look pretty good. Also the new algorithm runs in 1-2 minutes on the BWSN network.

--- wntr/metrics/__init__.py
+++ wntr/metrics/__init__.py
@@ -5,7 +5,8 @@ compute topographic metrics are included in the wntr.network.graph module.
 """
 from wntr.metrics.topographic import terminal_nodes, bridges, \
     central_point_dominance, spectral_gap, algebraic_connectivity, \
-    critical_ratio_defrag, valve_segments, valve_segment_attributes
+    critical_ratio_defrag, valve_segments, valve_segment_attributes, \
+        dev_valve_segments
 from wntr.metrics.hydraulic import expected_demand, average_expected_demand, \
     water_service_availability, todini_index, modified_resilience_index, \
     tank_capacity, entropy
