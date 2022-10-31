99d6ca9e67bbe7e0edbc2018740b89e10ad63b0c
Katherine Klise <kaklise@sandia.gov>
2022-10-31 16:10:19 -0700

    Added build input arg to setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import re
 import sys
 
 use_swig = False
-build = True
 
 extension_modules = list()
 
-# if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
-#     print("Python version >= 3.10.x")
-#     build = True
-
+if '--build' in sys.argv:
+    build = True
+    sys.argv.remove('--build')
+else:
+    build = False
+    
 if build:
     import numpy
 
