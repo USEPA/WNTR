def1a44420add5701b1bd43aa01a2170dc4e485e
Katherine Klise <kaklise@sandia.gov>
2023-02-02 14:04:55 -0800

    Added explanation on why this update is needed

--- wntr/gis/geospatial.py
+++ wntr/gis/geospatial.py
@@ -94,6 +94,7 @@ def snap(A, B, tolerance):
     closest["snap_distance"] = closest.geometry.distance(gpd.GeoSeries(closest.points, crs=crs))
         
     # Collect only point/link pairs within snap distance radius
+    # This is needed because B.sindex.intersection(row) above can return false positives
     closest = closest[closest['snap_distance'] <= tolerance]
     
     # Sort on ascending snap distance, so that closest goes to top
