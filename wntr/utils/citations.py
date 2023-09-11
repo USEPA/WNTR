d746e4fa1e60428ebbd727d0e388ff854d66de4d
dbhart <dbhart@sandia.gov>
2023-09-11 11:01:35 -0600

    Update documentation

--- wntr/utils/citations.py
+++ wntr/utils/citations.py
@@ -1,33 +1,170 @@
 # coding: utf-8
 """Contains a dataclass that can be used to store citations.
-Will output the class
 """
 import datetime
 from dataclasses import asdict, dataclass, field
 from typing import Any, Dict, Union, List, TypeAlias, Tuple
 
 Literal: TypeAlias = str
+"""A literal string"""
 Name: TypeAlias = str
+"""A name string, preferrably in order without commas"""
 NameList: TypeAlias = Union[Name, List[Name]]
+"""A list of names, or a string with "and" between names"""
 LiteralList: TypeAlias = Union[Literal, List[Literal]]
+"""A list of literals, or a string with "and" between literals"""
 Key: TypeAlias = str
+"""A string that comes from a limited set of values"""
 KeyList: TypeAlias = Union[Key, List[Key]]
+"""A list of keys, or a string with "and" between keys"""
 Verbatim: TypeAlias = str
+"""A string that should not be interpreted (ie raw)"""
 Special: TypeAlias = Any
+"""Anything that isn't a string or list of strings"""
 URI: TypeAlias = str
+"""A valid URI"""
 Date: TypeAlias = Union[str, datetime.date]
+"""A date or string in yyyy-mm-dd format (must be 0-padded, eg 2021-02-02 for February 2, 2021"""
 Range: TypeAlias = Union[str, Tuple[Any, Any]]
+"""A 2-tuple of (start,stop) or a string with two values separated by '--' """
 SeparatedValues: TypeAlias = Union[str, List[str]]
+"""A list of strings, or a string that is delimited (the delimiter is not specified)"""
 
 @dataclass(repr=False)
 class CitationFields:
     """A dataclass for citations, most attribute names match biblatex names.
-    The exceptions are :attr:`entry_type`, which is the "@..." type of an entry
-    :attr:`fulldate`, which is the "date" element of a bibliography entry
-    and :attr:`reporttype`, which is the "type" element of a bib(la)tex entry.
-
     This class makes no attempt to format or represent the citation in any form
     other than as a dictionary.
+
+    Parameters
+    ----------
+    title: Literal
+        The title is the only required field
+    author: NameList, optional
+    editor: NameList, optional
+    date: Date, optional
+    year: Literal, optional
+    abstract: Literal, optional
+    addendum: Literal, optional
+    afterword: NameList, optional
+    annotation: Literal, optional
+    annotator: NameList, optional
+    authortype: Key, optional
+    bookauthor: NameList, optional
+    bookpagination: Key, optional
+    chapter: Literal, optional
+    commentator: NameList, optional
+    doi: Verbatim, optional
+    edition: Union[int, Literal], optional
+    editora: NameList, optional
+    editorb: NameList, optional
+    editorc: NameList, optional
+    editortype: Key, optional
+    editoratype: Key, optional
+    editorbtype: Key, optional
+    editorctype: Key, optional
+    eid: Literal, optional
+    entrysubtype: Literal, optional
+    eprint: Verbatim, optional
+    eprintclass: Literal, optional
+    eprinttype: Literal, optional
+    eventdate: Date, optional
+    file: Verbatim, optional
+    foreword: NameList, optional
+    holder: NameList, optional
+    howpublished: Literal, optional
+    institution: LiteralList, optional
+    introduction: NameList, optional
+    isan: Literal, optional
+    isbn: Literal, optional
+    ismn: Literal, optional
+    isrn: Literal, optional
+    issn: Literal, optional
+    issue: Literal, optional
+    iswc: Literal, optional
+    eventtitle: Literal, optional
+    eventtitleaddon: Literal, optional
+    subtitle: Literal, optional
+    titleaddon: Literal, optional
+    journalsubtitle: Literal, optional
+    journaltitle: Literal, optional
+    journaltitleaddon: Literal, optional
+    issuesubtitle: Literal, optional
+    issuetitle: Literal, optional
+    issuetitleaddon: Literal, optional
+    booksubtitle: Literal, optional
+    booktitle: Literal, optional
+    booktitleaddon: Literal, optional
+    mainsubtitle: Literal, optional
+    maintitle: Literal, optional
+    maintitleaddon: Literal, optional
+    origtitle: Literal, optional
+    reprinttitle: Literal, optional
+    shorttitle: Literal, optional
+    shortjournal: Literal, optional
+    indextitle: Literal, optional
+    indexsorttitle: Literal, optional
+    sorttitle: Literal, optional
+    label: Literal, optional
+    language: KeyList, optional
+    library: Literal, optional
+    location: LiteralList, optional
+    month: Literal, optional
+    nameaddon: Literal, optional
+    note: Literal, optional
+    number: Literal, optional
+    organization: LiteralList, optional
+    origdate: Date, optional
+    origlanguage: KeyList, optional
+    origlocation: LiteralList, optional
+    origpublisher: LiteralList, optional
+    pages: Range, optional
+    pagetotal: Literal, optional
+    pagination: Key, optional
+    part: Literal, optional
+    publisher: LiteralList, optional
+    pubstate: Key, optional
+    series: Literal, optional
+    shortauthor: NameList, optional
+    shorteditor: NameList, optional
+    shorthand: Literal, optional
+    shorthandintro: Literal, optional
+    shortseries: Literal, optional
+    translator: NameList, optional
+    type: Key, optional
+    url: URI, optional
+    urldate: Date, optional
+    venue: Literal, optional
+    version: Literal, optional
+    volume: int, optional
+    volumes: int, optional
+    crossref: Key, optional
+    entryset: SeparatedValues, optional
+    execute: Special, optional
+    gender: Key, optional
+    langid: Key, optional
+    langidopts: Special, optional
+    ids: SeparatedValues, optional
+    keywords: SeparatedValues, optional
+    options: Special, optional
+    presort: Special, optional
+    related: SeparatedValues, optional
+    relatedoptions: SeparatedValues, optional
+    relatedtype: Special, optional
+    relatedstring: Literal, optional
+    sortkey: Literal, optional
+    sortname: NameList, optional
+    sortshorthand: Literal, optional
+    sortyear: int, optional
+    xdata: SeparatedValues, optional
+    xref: Key, optional
+    address: LiteralList, optional
+    annote: Literal, optional
+    archiveprefix: Literal, optional
+    journal: Literal, optional
+    pdf: Verbatim, optional
+    primaryclass: Literal, optional
+    school: LiteralList, optional
     """
     title: Literal
     author: NameList = None
@@ -190,6 +327,8 @@ class CitationFields:
 
 @dataclass(repr=False)
 class Citation:
+    """A class to hold a citation to document the source of a model.
+    """
     entry_type: str
     key: str
     fields: CitationFields = field(default_factory=CitationFields)
@@ -206,7 +345,7 @@ class Citation:
         Returns
         -------
         dict
-            the citation dictionaries
+            the citation dictionary
         """
         try:
             ret = dict()
