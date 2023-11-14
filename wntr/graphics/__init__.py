dadf6be4d9df5d984437f4cea7e981767b0c60de
kbonney <kirkb1998@gmail.com>
2023-11-14 09:51:41 -0500

    trivial commit to proc workflow

--- wntr/graphics/__init__.py
+++ wntr/graphics/__init__.py
@@ -6,3 +6,4 @@ from wntr.graphics.layer import plot_valve_layer
 from wntr.graphics.curve import plot_fragility_curve, plot_pump_curve, plot_tank_volume_curve
 from wntr.graphics.color import custom_colormap, random_colormap
 
+
