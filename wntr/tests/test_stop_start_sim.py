6930db97636f096785b6a7688dbd102854bc7843
kbonney <kirkb1998@gmail.com>
2023-11-17 17:47:19 -0500

    moved stopstart tests to own file

--- wntr/tests/test_stop_start_sim.py
+++ wntr/tests/test_stop_start_sim.py
@@ -1,13 +1,12 @@
-# These tests run a demand-driven simulation with both WNTR and Epanet and compare the results for the example networks
-import pickle
 import unittest
 from os.path import abspath, dirname, join
+import wntr
 
-import pandas as pd
 
 testdir = dirname(abspath(str(__file__)))
 test_datadir = join(testdir, "networks_for_testing")
 ex_datadir = join(testdir, "..", "..", "examples", "networks")
+
 
 tolerances = {
     "flowrate": 1.0e-5, # 10 mL/s
@@ -20,25 +19,32 @@ tolerances = {
     "setting": 1.0e-2,  # 0.01
 }
 
-class TestResetInitialValues(unittest.TestCase):
+class TestStopStartSim(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        import wntr
-
-        self.wntr = wntr
-        self.tol = 1e-8
-
         inp_file = join(ex_datadir, "Net3.inp")
-        self.wn = self.wntr.network.WaterNetworkModel(inp_file)
+        simulator = wntr.sim.WNTRSimulator
+        
+        # straight 24 hour simulation
+        self.wn = wntr.network.WaterNetworkModel(inp_file)
         self.wn.options.time.hydraulic_timestep = 3600
         self.wn.options.time.duration = 24 * 3600
-
-        sim = self.wntr.sim.WNTRSimulator(self.wn)
+        sim = simulator(self.wn)
         self.res1 = sim.run_sim(solver_options={"TOL": 1e-8})
-
-        self.wn.reset_initial_values()
-        self.res2 = sim.run_sim(solver_options={"TOL": 1e-8})
-
+        
+        # 24 hour simulation done in one 10 hour chunk and one 14 hour chunk
+        self.wn = wntr.network.WaterNetworkModel(inp_file)
+        self.wn.options.time.hydraulic_timestep = 3600
+        self.wn.options.time.duration = 10 * 3600
+        sim = simulator(self.wn)
+        self.res2 = sim.run_sim(solver_options={'TOL':1e-8})
+        self.wn.set_initial_conditions(self.res2)
+        self.wn.options.time.pattern_start = self.wn.options.time.pattern_start + 10 * 3600
+        self.wn.options.time.duration = 14 * 3600
+        sim = simulator(self.wn)
+        self.res3 = sim.run_sim(solver_options={'TOL':1e-8})
+        self.res3._adjust_time(10*3600)
+        self.res2.append(self.res3)
         self.res4 = abs(self.res1 - self.res2).max()
 
     @classmethod
@@ -47,6 +53,7 @@ class TestResetInitialValues(unittest.TestCase):
 
     def test_link_flowrate(self):
         for link_name, link in self.wn.links():
+            print(link_name, self.res1.link["flowrate"].loc[:,link_name].describe(), abs(self.res1 - self.res2).link["flowrate"].loc[:,link_name].describe())
             self.assertLess(
                 self.res4.link["flowrate"].at[link_name],
                 tolerances["flowrate"],
@@ -81,40 +88,31 @@ class TestResetInitialValues(unittest.TestCase):
             )
 
 
-class TestPickle(unittest.TestCase):
+
+class TestStopStartEpanetSim(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        import wntr
-
-        self.wntr = wntr
-
         inp_file = join(ex_datadir, "Net3.inp")
-
-        parser = self.wntr.epanet.InpFile()
-        self.wn = parser.read(inp_file)
+        
+        # straight 24 hour simulation
+        self.wn = wntr.network.WaterNetworkModel(inp_file)
         self.wn.options.time.hydraulic_timestep = 3600
         self.wn.options.time.duration = 24 * 3600
-        sim = self.wntr.sim.WNTRSimulator(self.wn)
-        self.res1 = sim.run_sim(solver_options={"TOL": 1e-8})
-
-        parser = self.wntr.epanet.InpFile()
-        self.wn2 = parser.read(inp_file)
-        self.wn2.options.time.hydraulic_timestep = 3600
-        self.wn2.options.time.duration = 10 * 3600
-        sim = self.wntr.sim.WNTRSimulator(self.wn2)
-        self.res2 = sim.run_sim(solver_options={"TOL": 1e-8})
-        self.wn2.set_initial_conditions(self.res2, 36000)
-        self.wn2.options.time.pattern_start = self.wn2.options.time.pattern_start + 10 * 3600
-        f = open("temp.pickle", "wb")
-        pickle.dump(self.wn2, f)
-        f.close()
-        f = open("temp.pickle", "rb")
-        self.wn2 = pickle.load(f)
-        f.close()
-        self.wn2.options.time.duration = 14*3600
-        sim = self.wntr.sim.WNTRSimulator(self.wn2)
-        self.res3 = sim.run_sim(solver_options={'TOL':1e-8})
-        self.res3._adjust_time(10*3600)
+        sim = wntr.sim.EpanetSimulator(self.wn)
+        self.res1 = sim.run_sim()
+        
+        # 24 hour simulation done in one 10 hour chunk and one 14 hour chunk
+        self.wn = wntr.network.WaterNetworkModel(inp_file)
+        self.wn.options.time.hydraulic_timestep = 3600
+        self.wn.options.time.duration = 11 * 3600
+        sim = wntr.sim.EpanetSimulator(self.wn)
+        self.res2 = sim.run_sim()
+        self.wn.set_initial_conditions(self.res2)
+        self.wn.options.time.pattern_start = self.wn.options.time.pattern_start + 11 * 3600
+        self.wn.options.time.duration = 13 * 3600
+        sim = wntr.sim.EpanetSimulator(self.wn)
+        self.res3 = sim.run_sim()
+        self.res3._adjust_time(11*3600)
         
         self.res2.append(self.res3)
         self.res4 = abs(self.res1 - self.res2).max()
@@ -122,7 +120,6 @@ class TestPickle(unittest.TestCase):
     @classmethod
     def tearDownClass(self):
         pass
-
 
     def test_link_flowrate(self):
         for link_name, link in self.wn.links():
@@ -136,6 +133,27 @@ class TestPickle(unittest.TestCase):
             self.assertLess(
                 self.res4.link["velocity"].at[link_name],
                 tolerances["velocity"]
+            )
+
+    def test_link_headloss(self):
+        for link_name, link in self.wn.links():
+            self.assertLess(
+                self.res4.link["headloss"].at[link_name],
+                tolerances["headloss"]
+            )
+
+    def test_link_status(self):
+        for link_name, link in self.wn.links():
+            self.assertLess(
+                self.res4.link["status"].at[link_name],
+                tolerances["status"]
+            )
+
+    def test_link_setting(self):
+        for link_name, link in self.wn.links():
+            self.assertLess(
+                self.res4.link["setting"].at[link_name],
+                tolerances["setting"]
             )
 
     def test_node_demand(self):
