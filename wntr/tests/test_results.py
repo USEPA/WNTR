6069f2d99844bc46641ddf522c8ac6443deb432b
kbonney <kirkb1998@gmail.com>
2023-11-20 11:04:56 -0500

    update test results

--- wntr/tests/test_results.py
+++ wntr/tests/test_results.py
@@ -2,6 +2,7 @@
 import copy
 import unittest
 from os.path import abspath, dirname, join
+from pandas.testing import assert_frame_equal, assert_series_equal, assert_index_equal
 
 import pandas as pd
 
@@ -45,31 +46,87 @@ class TestResultsOperations(unittest.TestCase):
         res_copy = copy.deepcopy(self.res)
         res_copy._adjust_time(self.ts)
         
-        for key in self.res.link.keys():
-            assert self.res.link[key].index + self.ts == res_copy.node[key].index
-        for key in self.res.node.keys():
-            assert self.res.node[key].index + self.ts == res_copy.node[key].index
+        for attr in self.res._data_attributes:
+            for key in getattr(self.res, attr).keys():
+                assert_index_equal(getattr(self.res, attr)[key].index + self.ts, getattr(res_copy, attr)[key].index)
 
     def test_append(self):
         res_copy1 = copy.deepcopy(self.res)
         res_copy2 = copy.deepcopy(self.res)
         res_copy2._adjust_time(self.wn.options.time.duration)
         res_copy1.append(res_copy2)
-        pass
+        assert res_copy1.node["head"].shape == (9,97)
+        assert (res_copy1.node["head"].loc[0] == res_copy1.node["head"].loc[14400]).all()
     
     def test_arithmetic(self):
-        added_res = self.res + self.res
-        subtracted_res = self.res - self.res
-        divided_res = self.res / self.res
-        int_divided_res = self.res / 2
-        pow_res = pow(self.res, 1/2)
-        abs_res = abs(self.res)
-        neg_res = -self.res
-        pos_res = +self.res
-        max_res = self.res.max()
-        min_res = self.res.min()
-        summed_res = self.res.sum()
-        foo
+        df = self.res.node["head"]
+        
+        # addition
+        temp_res = self.res + self.res
+        temp_df = temp_res.node["head"]
+        test_df = df + df
+        assert_frame_equal(temp_df, test_df)
+        
+        # subtraction
+        temp_res = self.res - self.res
+        temp_df = temp_res.node["head"]
+        test_df = df - df
+        assert_frame_equal(temp_df, test_df)
+        
+        # division
+        temp_res = self.res / self.res
+        temp_df = temp_res.node["head"]
+        test_df = df / df
+        assert_frame_equal(temp_df, test_df)
+        
+        # int division
+        temp_res = self.res / 2
+        temp_df = temp_res.node["head"]
+        test_df = df / 2
+        assert_frame_equal(temp_df, test_df)
+        
+        # power
+        temp_res = pow(self.res, 1/2)
+        temp_df = temp_res.node["head"]
+        test_df = pow(df, 1/2)
+        assert_frame_equal(temp_df, test_df)
+        
+        # abs
+        temp_res = abs(self.res)
+        temp_df = temp_res.node["head"]
+        test_df = abs(df)
+        assert_frame_equal(temp_df, test_df)
+        
+        # neg
+        temp_res = -self.res
+        temp_df = temp_res.node["head"]
+        test_df = -df
+        assert_frame_equal(temp_df, test_df)
+        
+        # pos
+        temp_res = +self.res
+        temp_df = temp_res.node["head"]
+        test_df = +df
+        assert_frame_equal(temp_df, test_df)
+        
+        # max
+        temp_res = self.res.max()
+        temp_df = temp_res.node["head"]
+        test_df = df.max(axis=0)
+        assert_series_equal(temp_df, test_df)
+        
+        # min
+        temp_res = self.res.min()
+        temp_df = temp_res.node["head"]
+        test_df = df.min(axis=0)
+        assert_series_equal(temp_df, test_df)
+        
+        # sum
+        temp_res = self.res.sum()
+        temp_df = temp_res.node["head"]
+        test_df = df.sum(axis=0)
+        assert_series_equal(temp_df, test_df)
+
 
 
 if __name__ == "__main__":
