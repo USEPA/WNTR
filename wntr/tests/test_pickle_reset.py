a7272a66c10bb22c52cfdca3b84cdc5a97fce8fb
kbonney <kirkb1998@gmail.com>
2023-11-20 11:20:46 -0500

    updating test pickle

--- wntr/tests/test_pickle_reset.py
+++ wntr/tests/test_pickle_reset.py
@@ -1,4 +1,5 @@
 # Test the use of pickling and then unpickling a water network object for resetting values
+# Also tests pickling and unpickling in the middle of a simulation
 import pickle
 import unittest
 from os.path import abspath, dirname, join
@@ -30,22 +31,37 @@ class TestPickle(unittest.TestCase):
         self.wn = wntr.network.WaterNetworkModel(inp_file)
         self.wn.options.time.hydraulic_timestep = 3600
         self.wn.options.time.duration = 24 * 3600
+        
+        # pickle WN before running sims
+        f = open("temp.pickle", "wb")
+        pickle.dump(self.wn, f)
+        f.close()
+
+        
+        # run sim with initial wn
         sim = wntr.sim.WNTRSimulator(self.wn)
         self.res1 = sim.run_sim(solver_options={"TOL": 1e-8})
+        
+        # load pickled inital wn
+        f = open("temp.pickle", "rb")
+        self.wn2 = pickle.load(f)
+        f.close()
 
-        self.wn2 = wntr.network.WaterNetworkModel(inp_file)
         self.wn2.options.time.hydraulic_timestep = 3600
         self.wn2.options.time.duration = 10 * 3600
         sim = wntr.sim.WNTRSimulator(self.wn2)
         self.res2 = sim.run_sim(solver_options={"TOL": 1e-8})
         self.wn2.set_initial_conditions(self.res2, 36000)
         self.wn2.options.time.pattern_start = self.wn2.options.time.pattern_start + 10 * 3600
+        
+        # pickle wn and reload and set conditions from previous sim
         f = open("temp.pickle", "wb")
         pickle.dump(self.wn2, f)
         f.close()
         f = open("temp.pickle", "rb")
         self.wn2 = pickle.load(f)
         f.close()
+        
         self.wn2.options.time.duration = 14*3600
         sim = wntr.sim.WNTRSimulator(self.wn2)
         self.res3 = sim.run_sim(solver_options={'TOL':1e-8})
