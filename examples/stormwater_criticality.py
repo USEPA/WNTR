1810d8736d7c6b68ddf944d745e54c6be8862bb6
Katherine Klise <kaklise@sandia.gov>
2024-01-23 09:10:21 -0800

    clean up examples

--- examples/stormwater_criticality.py
+++ examples/stormwater_criticality.py
@@ -2,7 +2,7 @@
 import matplotlib.pylab as plt
 import wntr.stormwater as swntr
 
-inp_file = 'networks/Example.inp'
+inp_file = 'networks/Site_Drainage_Model.inp'
 swn = swntr.network.StormWaterNetworkModel(inp_file)
 
 results = {}
@@ -11,15 +11,11 @@ fig, ax = plt.subplots()
 for conduit_name in swn.conduit_name_list:
     print(conduit_name)
     swn = swntr.network.StormWaterNetworkModel(inp_file)
-    swn.conduits.loc[conduit_name, "MaxFlow"] = 0.0000001
+    swn.conduits.loc[conduit_name, "MaxFlow"] = 0.00001
     sim = swntr.sim.SWMMSimulator(swn)
     results[conduit_name] = sim.run_sim(conduit_name)
 
     flowrate = results[conduit_name].link['FLOW_VELOCITY']
-    #swntr.graphics.plot_network(swn, 
-    #                            link_attribute=flowrate.mean(axis=0), 
-    #                            link_width=2, 
-    #                            link_range=[0,1])
-    flowrate.mean(axis=1).plot(ax=ax)
+    flowrate.mean(axis=1).plot(ax=ax, label=conduit_name)
     
-print(results['C1'].node.keys())
+plt.legend()
