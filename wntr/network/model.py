99aa5663b1cd34110d2b4ac0192c4d4629ca160b
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2024-11-29 17:07:41 -0500

    testing for links

--- wntr/network/model.py
+++ wntr/network/model.py
@@ -1209,10 +1209,11 @@ class WaterNetworkModel(AbstractModel):
     def from_gis(self, gis_data):
         """
         Append the model with elements from GeoDataFrames
+        :class:`~geopandas.GeoDataFrame`
         
         Parameters
         ----------
-        gis_data : WaterNetworkGIS or dict of geopandas.GeoDataFrame
+        gis_data : WaterNetworkGIS or dict of geoPandas.GeoDataFrame
             GeoDataFrames containing water network attributes. If gis_data is a 
             dictionary, then the keys are junctions, tanks, reservoirs, pipes, 
             pumps, and valves. If the pumps or valves are Points, they will be 
