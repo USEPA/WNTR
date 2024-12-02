37795614da665c1e118dcfc6975a4c5946080b46
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2024-12-02 12:32:38 -0500

    Attempting to link "to_graph" under "get_graph"

--- wntr/network/model.py
+++ wntr/network/model.py
@@ -1255,7 +1255,9 @@ class WaterNetworkModel(AbstractModel):
         
         .. deprecated:: 0.5.0
         Use :ref:`to_graph() <wntr.network.model.to_graph>` instead
-        :meth:`to_graph()' <wntr.network.model.to_graph>`
+        :meth:`to_graph() <wntr.network.model.to_graph>`
+        :def:`to_graph() <wntr.network.model.to_graph>`
+        :funct:`to_graph() <wntr.network.model.to_graph>`
         
         Parameters
         ----------
