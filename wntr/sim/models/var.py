07ef506c9b0c591a6f8e93c887fbe94eb6ff2abe
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2025-01-14 17:43:00 -0500

    Linking “wntr.sim.aml.aml.Model” under parameter m

--- wntr/sim/models/var.py
+++ wntr/sim/models/var.py
@@ -12,7 +12,7 @@ def demand_var(m, wn, index_over=None):
 
     Parameters
     ----------
-    m: wntr.aml.aml.aml.Model
+    m: wntr.sim.aml.aml.Model
     wn: wntr.network.model.WaterNetworkModel
     index_over: list of str
         list of junction names
