a09bd9973f26c669589c551bb08d5dd539f2e237
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2025-01-15 15:21:49 -0500

    Linking “wntr.sim.aml.aml.Model” in parameter m under flow_var

--- wntr/sim/models/var.py
+++ wntr/sim/models/var.py
@@ -37,7 +37,7 @@ def flow_var(m, wn, index_over=None):
 
     Parameters
     ----------
-    m: wntr.aml.aml.aml.Model
+    m: wntr.sim.aml.aml.Model
     wn: wntr.network.model.WaterNetworkModel
     index_over: list of str
         list of link names
