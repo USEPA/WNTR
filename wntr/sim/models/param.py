7a4d209526fc7ab5459d28b72099b289fad6fecc
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2025-01-15 17:49:19 -0500

    Linking “wntr.sim.aml.aml.Model” in parameter m under classmethod build

--- wntr/sim/models/param.py
+++ wntr/sim/models/param.py
@@ -317,7 +317,7 @@ class elevation_param(Definition):
 
         Parameters
         ----------
-        m: wntr.aml.aml.aml.Model
+        m: wntr.sim.aml.aml.Model
         wn: wntr.network.model.WaterNetworkModel
         updater: ModelUpdater
         index_over: list of str
