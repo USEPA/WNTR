a88837ca8c130b110419f9f33097ca254f305b72
Wendy Dushanin <79654823+wDushanin@users.noreply.github.com>
2025-01-16 14:17:45 -0500

    Linking “wntr.sim.aml.aml.Model” in parameter m under classmethod build

--- wntr/sim/models/constraint.py
+++ wntr/sim/models/constraint.py
@@ -393,7 +393,7 @@ class prv_headloss_constraint(Definition):
 
         Parameters
         ----------
-        m: wntr.aml.aml.aml.Model
+        m: wntr.sim.aml.aml.Model
         wn: wntr.network.model.WaterNetworkModel
         updater: ModelUpdater
         index_over: list of str
