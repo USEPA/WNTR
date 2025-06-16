8d202aa9c5bbab649279eb51157386a9dc712996
kbonney <kirkb1998@gmail.com>
2025-06-16 10:29:12 -0400

    update tests

--- wntr/tests/test_library_model.py
+++ wntr/tests/test_library_model.py
@@ -3,37 +3,38 @@ import os
 import shutil
 from os.path import abspath, dirname, join
 from wntr.network import WaterNetworkModel
-from wntr.library import ModelLibrary
+from wntr.library import ModelLibrary, model_library
 
 testdir = dirname(abspath(str(__file__)))
 test_datadir = join(testdir, "networks_for_testing")
 ex_datadir = join(testdir, "..", "..", "examples", "networks")
 
 class TestModelLibrary(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        self.library = ModelLibrary()
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-    
     def test_model_name_list(self):
-        self.assertIn("Net3", self.library.model_name_list)
-        self.assertIn("ky4", self.library.model_name_list)
+        self.assertIn("Net3", model_library.model_name_list)
+        self.assertIn("ky4", model_library.model_name_list)
     
     def test_get_model(self):
-        model = self.library.get_model("ky4")
+        model = model_library.get_model("ky4")
         self.assertIsInstance(model, WaterNetworkModel)
+        
+    def test_model_construction(self):
+        # Test construction from name
+        model = WaterNetworkModel("ky4")
         
     def test_add_directory(self):
         # Test adding a directory
-        self.library.add_directory(test_datadir)
-        self.assertIn("io", self.library.model_name_list)
+        model_library.add_directory(test_datadir)
+        self.assertIn("io", model_library.model_name_list)
         
         # Test getting the filepath after adding a directory
-        filepath = self.library.get_filepath("io")
+        filepath = model_library.get_filepath("io")
         self.assertEqual(filepath, join(test_datadir, "io.inp"))
+        
+        # Test construction from file path
+        model = WaterNetworkModel("io")
+        self.assertIsInstance(model, WaterNetworkModel)
+
 
 if __name__ == "__main__":
     unittest.main()
