a1520f996a0ac9a0263fc00fb8ec7fadbf59d8bc
kbonney <kirkb1998@gmail.com>
2025-10-24 20:09:17 -0400

    add dynamic version

--- setup.py
+++ setup.py
@@ -79,15 +79,11 @@ file_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(file_dir, 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
-# get version from __init__.py
-with open(os.path.join(file_dir, 'wntr', '__init__.py')) as f:
-    version_file = f.read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        VERSION = version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
+import datetime
+
+# Generate a unique version based on timestamp
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+VERSION = f"0.0.1.dev{timestamp}"
 
 print(extension_modules)
 
