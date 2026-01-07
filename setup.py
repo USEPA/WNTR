f7281cf8dfdb1c051467b06cd765ff265b8b311d
kbonney <kirkb1998@gmail.com>
2026-01-07 16:27:05 -0500

    cap pandas requirement below 3.0

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ AUTHOR = 'WNTR Developers'
 MAINTAINER_EMAIL = 'kaklise@sandia.gov'
 LICENSE = 'Revised BSD'
 URL = 'https://github.com/USEPA/WNTR'
-DEPENDENCIES = ['numpy>=2.2.6 ', 'scipy', 'networkx', 'pandas', 'matplotlib', 'setuptools']
+DEPENDENCIES = ['numpy>=2.2.6 ', 'scipy', 'networkx', 'pandas<3.0', 'matplotlib', 'setuptools']
 
 # use README file as the long description
 file_dir = os.path.abspath(os.path.dirname(__file__))
